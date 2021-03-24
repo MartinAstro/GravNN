Fixed the potential calculation for spherical harm

GravNN/GravityModels/PinesAlgorithm.py
@@ -79,15 +79,19 @@ def compute_acceleration(positions, N, mu, a, n1, n2, n1q, n2q, cbar, sbar):
 @njit(parallel=True, cache=True) 
 def compute_acc_parallel(positions, N, mu, a, n1, n2, n1q, n2q, cbar, sbar):
     acc = np.zeros(positions.shape)
+    potential = np.zeros((int(len(positions)/3),))
     if N == -1:
-        return acc
+        return (acc, potential)
     for i in prange(0, int(len(positions)/3)):
-        acc[3*i:3*(i+1)] = compute_acc_thread(positions[3*i:3*(i+1)], N, mu, a, n1, n2, n1q, n2q, cbar, sbar)
-    return acc
+        results = compute_acc_thread(positions[3*i:3*(i+1)], N, mu, a, n1, n2, n1q, n2q, cbar, sbar) 
+        acc[3*i:3*(i+1)] = results[0]
+        potential[i] = results[1]
+    return (acc, potential)
     
 @njit(cache=True)
 def compute_acc_thread(position, N, mu, a, n1, n2, n1q, n2q, cbar, sbar):
     acc = np.zeros(position.shape)
+    potential = 0.0
     r = np.linalg.norm(position)
     [s, t, u] = position/r
 
@@ -128,10 +132,15 @@ def compute_acc_thread(position, N, mu, a, n1, n2, n1q, n2q, cbar, sbar):
             if m < l:
                 sum_a3 += n1q[l][m]*aBar[l][m+1]*D
             sum_a4 += n2q[l][m]*aBar[l+1][m+1]*D
+
+            potential += rhol[l]*aBar[l][m]*D
         a1 += rhol[l+1]/a*sum_a1
         a2 += rhol[l+1]/a*sum_a2
         a3 += rhol[l+1]/a*sum_a3
         a4 -= rhol[l+1]/a*sum_a4
     a4 -= rhol[1]/a
 
-    return (np.array([a1, a2, a3]) + np.array([s,t,u])*a4)#.tolist()
+    # The prior loop doesn't account for the l=0 index
+    potential += (rhol[0]*aBar[0][0]*(cbar[0][0]*rE[0] + sbar[0][0]*iM[0]))
+
+    return (np.array([a1, a2, a3]) + np.array([s,t,u])*a4, potential)

GravNN/GravityModels/SphericalHarmonics.py
@@ -31,14 +31,6 @@ def get_normalization(l, m):
                 N[i,j] = np.sqrt((2*(2*i+1)*np.math.factorial(i-j))/(np.math.factorial(i+j)))
     return N
 
-#@njit(cache=True)
-def get_potential(legendre, l, m, Nlm, Clm, Slm, R, mu, r, lambda_, phi_):
-    U = 0.0 # Potential
-    for i in range(0, l+1):
-        for j in range(0, i+1):
-            U += mu/r*(R/r)**i *Nlm[i,j] * legendre[0][j,i] * (Clm[i,j]*np.cos(j*lambda_) + Slm[i,j]*np.sin(j*lambda_))
-    return U
-
 def get_sh_data(trajectory, gravity_file, **kwargs):
 
     # Handle cases where the keyword wasn't properly wrapped as a list []
@@ -61,7 +53,7 @@ def get_sh_data(trajectory, gravity_file, **kwargs):
 
     # By default the potential isn't loaded into the training data
     if 'use_potential' in kwargs:
-        if kwargs['use_potential'][0]
+        if kwargs['use_potential'][0]:
             potentials = Call_r0_gm.potentials
             potentials_Clm = Clm_r0_gm.potentials
             u = potentials - potentials_Clm
@@ -190,15 +182,15 @@ class SphericalHarmonics(GravityModelBase):
         if positions is None:
             positions = self.trajectory.positions
         
-        positions_sph = cart2sph(positions) # [r, lambda, phi]
-        self.potential = np.zeros((len(positions),))
-        l = self.degree
-        m = l
-        Nlm = get_normalization(l,m)
-        for i in range(len(positions)):
-            legendre = lpmn(m, l, np.sin(positions_sph[i,2]))
-            self.potential[i] = get_potential(legendre, l, m, Nlm, self.C_lm, self.S_lm, self.radEquator, self.mu, positions_sph[i,0], np.deg2rad(positions_sph[i,1]), np.deg2rad(positions_sph[i,2]))
-        return self.potential
+        positions = np.reshape(positions, (len(positions)*3))
+        
+        n1, n2, n1q, n2q = compute_n_matrices(self.degree)
+        accelerations, potentials = compute_acc_parallel(positions, self.degree, self.mu, self.radEquator, n1, n2, n1q, n2q, self.C_lm, self.S_lm)      
+        
+        self.accelerations = np.reshape(np.array(accelerations), (int(len(np.array(accelerations))/3), 3))
+        self.potentials = potentials
+
+        return self.potentials
 
     
     def compute_acceleration(self, positions=None):
@@ -209,9 +201,10 @@ class SphericalHarmonics(GravityModelBase):
         positions = np.reshape(positions, (len(positions)*3))
         
         n1, n2, n1q, n2q = compute_n_matrices(self.degree)
-        accelerations = compute_acc_parallel(positions, self.degree, self.mu, self.radEquator, n1, n2, n1q, n2q, self.C_lm, self.S_lm)      
+        accelerations, potentials = compute_acc_parallel(positions, self.degree, self.mu, self.radEquator, n1, n2, n1q, n2q, self.C_lm, self.S_lm)      
         
         self.accelerations = np.reshape(np.array(accelerations), (int(len(np.array(accelerations))/3), 3))
+        self.potentials = potentials
         return self.accelerations
 
 