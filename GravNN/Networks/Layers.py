eb2716f80aa455eddfc9250abd4fe1ed0e90616e
John M <john.r.martin@colorado.edu>
2022-12-19 15:57:11 -0700

    Refactor old and new custom layers

--- GravNN/Networks/Layers.py
+++ GravNN/Networks/Layers.py
@@ -3,48 +3,8 @@ import tensorflow as tf
 import numpy as np
 
 
-class PreprocessingLayer(tf.keras.layers.Layer):
-    def __init__(self, min, scale, dtype):
-        super(PreprocessingLayer, self).__init__(dtype=dtype)
-        self.min = tf.constant(min, dtype=dtype)
-        self.scale = tf.constant(scale, dtype=dtype)
-
-    def call(self, inputs):
-        normalized_inputs = inputs * self.scale + self.min
-        return normalized_inputs
-
-    def get_config(self):
-        config = super().get_config().copy()
-        config.update(
-            {
-                "min": self.min,
-                "scale": self.scale,
-            }
-        )
-        return config
-
-class PostprocessingLayer(tf.keras.layers.Layer):
-    def __init__(self, min, scale, dtype):
-        super(PostprocessingLayer, self).__init__(dtype=dtype)
-        self.min = tf.constant(min, dtype=dtype)
-        self.scale = tf.constant(scale, dtype=dtype)
-
-    def call(self, normalized_inputs):
-        inputs = (normalized_inputs - self.min)/self.scale
-        return inputs
-
-    def get_config(self):
-        config = super().get_config().copy()
-        config.update(
-            {
-                "min": self.min,
-                "scale": self.scale,
-            }
-        )
-        return config
-
 class Cart2PinesSphLayer(tf.keras.layers.Layer):
-    def __init__(self, dtype):
+    def __init__(self, dtype, **kwargs):
         """Successor to the Cart2SphLayer. The layer takes a
         cartesian input and transforms it into a non-singular spherical
         representation (see Pines formulation). This bypasses the singularity introduced at the pole
@@ -77,95 +37,20 @@ class Cart2PinesSphLayer(tf.keras.layers.Layer):
         config = super().get_config().copy()
         return config
 
-class PinesSph2NetLayer(tf.keras.layers.Layer):
-    def __init__(self, dtype, scalers, mins, ref_radius):
-        """Layer used to normalize the r value of the Cart2PinesSphLayer
-        between the values of [-1,1]
+class NormalizeRLayer(tf.keras.layers.Layer):
+    def __init__(self, dtype, 
+        ref_radius_min, ref_radius_max,
+        feature_min, feature_max, **kwargs
+        ):
+        super(NormalizeRLayer, self).__init__(dtype=dtype)
 
-        Args:
-                scalers (np.array): values used to scale each input quantity. These are computed before initialization
-                mins (np.array): values used to bias each input before scaling. These are computed before initialization
-        """
-        super(PinesSph2NetLayer, self).__init__(dtype=dtype)
-        self.scalers = tf.constant(scalers, dtype=dtype).numpy()
-        self.mins = tf.constant(mins, dtype=dtype).numpy()
-        self.ref_radius = tf.constant(ref_radius, dtype=dtype).numpy()
-
-    def call(self, inputs):
-        inputs_transpose = tf.transpose(inputs)
-        r = inputs_transpose[0]
-        beta = inputs_transpose[1]
-        gamma = inputs_transpose[2]
-        alpha = inputs_transpose[3]
-
-        # Normalize r -> [-1,1]
-        r = tf.add(tf.multiply(r, self.scalers[0]), self.mins[0])
-        spheres = tf.stack([r, beta, gamma, alpha], axis=1)
-        return spheres
-
-    def get_config(self):
-        config = super().get_config().copy()
-
-        config.update(
-            {
-                "scalers": self.scalers,
-                "mins": self.mins,
-                "ref_radius":self.ref_radius
-            }
-        )
-        config = super().get_config().copy()
-        return config
-
-class PinesSph2NetRefLayer(tf.keras.layers.Layer):
-    def __init__(self, dtype, scalers, mins, ref_radius):
-        """Layer used to normalize the r value of the Cart2PinesSphLayer
-        between the values of [-1,1]
-
-        Args:
-                scalers (np.array): values used to scale each input quantity. These are computed before initialization
-                mins (np.array): values used to bias each input before scaling. These are computed before initialization
-        """
-        super(PinesSph2NetRefLayer, self).__init__(dtype=dtype)
-        self.ref_radius = ref_radius
-
-    def call(self, inputs):
-        inputs_transpose = tf.transpose(inputs)
-        r = inputs_transpose[0]
-        beta = inputs_transpose[1]
-        gamma = inputs_transpose[2]
-        alpha = inputs_transpose[3]
-
-        # Normalize r -> [0,inf]
-        r_inv_prime = tf.divide(self.ref_radius,r)
-        spheres = tf.stack([r_inv_prime, beta, gamma, alpha], axis=1)
-        return spheres
-
-    def get_config(self):
-        config = super().get_config().copy()
-
-        config.update(
-            {
-                "ref_radius":self.ref_radius
-            }
-        )
-        config = super().get_config().copy()
-        return config
-
-class PinesSph2NetLayer_v2(tf.keras.layers.Layer):
-    def __init__(self, dtype, ref_radius_min=None, ref_radius_max=None):
-        super(PinesSph2NetLayer_v2, self).__init__(dtype=dtype)
-        self.ref_radius_min = tf.cond(ref_radius_min != None, 
-                        lambda : tf.constant(ref_radius_min, dtype=dtype), 
-                        lambda : tf.constant(1E-3, dtype=dtype)).numpy() 
-                        # Don't set min to zero otherwise nans
-        self.ref_radius_max = tf.cond(ref_radius_max != None, 
-                        lambda : tf.constant(ref_radius_max, dtype=dtype), 
-                        lambda : tf.constant(1.0, dtype=dtype)).numpy()
-        
+        self.ref_radius_min = tf.constant(ref_radius_min, dtype=dtype).numpy()
+        self.ref_radius_max = tf.constant(ref_radius_max, dtype=dtype).numpy()
+  
         # bounds of the feature -- shouldn't necessarily be a large difference.
         # think about how much the output should change with respect to the inputs
-        self.r_star_max = tf.constant(1.0 + (self.ref_radius_max - self.ref_radius_min)/self.ref_radius_min, dtype=dtype).numpy()           
-        self.r_star_min = tf.constant(1.0, dtype=dtype).numpy()
+        self.feature_min = tf.constant(feature_min, dtype=dtype).numpy()
+        self.feature_max = tf.constant(feature_max, dtype=dtype).numpy()           
 
     def call(self, inputs):
         inputs_transpose = tf.transpose(inputs)
@@ -174,16 +59,13 @@ class PinesSph2NetLayer_v2(tf.keras.layers.Layer):
         t = inputs_transpose[2]
         u = inputs_transpose[3]
 
-        ds = self.r_star_max - self.r_star_min
+        df = self.feature_max - self.feature_min
         dr = self.ref_radius_max - self.ref_radius_min
-        scale = tf.divide(ds, dr)
-        min_arg = self.r_star_min - tf.multiply(self.ref_radius_min, scale)
-        r_star = tf.multiply(r, scale) + min_arg
+        scale = tf.divide(df, dr)
+        min_arg = self.feature_min - tf.multiply(self.ref_radius_min, scale)
+        feature = tf.multiply(r, scale) + min_arg
 
-        one = tf.constant(1.0, dtype=r.dtype)
-        r_inv = tf.divide(one, r_star)
-        # r_inv = r_star
-        spheres = tf.stack([r_inv, s, t, u], axis=1)
+        spheres = tf.stack([feature, s, t, u], axis=1)
         return spheres
 
     def get_config(self):
@@ -191,20 +73,59 @@ class PinesSph2NetLayer_v2(tf.keras.layers.Layer):
         config.update(
             {
                 "dtype" : self.dtype,
-                "r_star_min" : self.r_star_min,
-                "r_star_max" : self.r_star_max,
+                "feature_min" : self.feature_min,
+                "feature_max" : self.feature_max,
                 "ref_radius_min" : self.ref_radius_min,
                 "ref_radius_max" : self.ref_radius_max
             }
         )
         return config
 
+
+class ScaleRLayer(tf.keras.layers.Layer):
+    def __init__(self, dtype, ref_radius_max, **kwargs):
+        super(ScaleRLayer, self).__init__(dtype=dtype)
+        self.ref_radius_max = tf.constant(ref_radius_max, dtype=dtype)
+
+    def call(self, inputs):
+        inputs_transpose = tf.transpose(inputs)
+        r = inputs_transpose[0]
+        s = inputs_transpose[1]
+        t = inputs_transpose[2]
+        u = inputs_transpose[3]
+        r_star = tf.divide(r, self.ref_radius_max)
+        spheres = tf.stack([r_star, s, t, u], axis=1)
+        return spheres
+
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update({ "ref_radius_max" : self.ref_radius_max})
+        return config
+
+class InvRLayer(tf.keras.layers.Layer):
+    def __init__(self, dtype, **kwargs):
+        super(InvRLayer, self).__init__(dtype=dtype)
+
+    def call(self, inputs):
+        inputs_transpose = tf.transpose(inputs)
+        r = inputs_transpose[0]
+        s = inputs_transpose[1]
+        t = inputs_transpose[2]
+        u = inputs_transpose[3]
+
+        one = tf.constant(1.0, dtype=r.dtype)
+        r_inv = tf.divide(one, r)
+        spheres = tf.stack([r_inv, s, t, u], axis=1)
+        return spheres
+
+
+
 class FourierFeatureLayer(tf.keras.layers.Layer):
-    def __init__(self, M_features, sigma, scale):
-        super(FourierFeatureLayer, self).__init__()
+    def __init__(self, fourier_features, fourier_sigma, fourier_scale, **kwargs):
+        super(FourierFeatureLayer, self).__init__(dtype=kwargs.get('dtype'))
         # self.a = np.ones((len(M),)).astype(self.dtype)#
         # self.a = np.diag(np.array([1.0/j for j in range(1,M+1)], dtype=self.dtype))
-        self.B = np.random.normal(0, sigma, size=(M_features, 3)).astype(self.dtype) * scale
+        self.B = np.random.normal(0, fourier_sigma, size=(fourier_features, 3)).astype(self.dtype) * fourier_scale
 
     def call(self, inputs):
         inputs_transpose = tf.transpose(inputs) # [4 x N]
@@ -245,27 +166,8 @@ class FourierFeatureLayer(tf.keras.layers.Layer):
         )
         return config
 
-
-class PointMassLayer(tf.keras.layers.Layer):
-    def __init__(self, dtype, mu, r_max):
-        super(PointMassLayer, self).__init__(dtype=dtype)
-        self.mu = tf.constant(mu, dtype=dtype).numpy()
-
-    def call(self, inputs):
-        r = tf.linalg.norm(inputs, axis=1, keepdims=True)
-        u_pm = tf.negative(tf.divide(self.mu,r))
-        return u_pm
-
-    def get_config(self):
-        config = super().get_config().copy()
-        config.update(
-            {
-                "mu" : self.mu,
-            }
-        )
-        return config
-
 class BlendPotentialLayer(tf.keras.layers.Layer):
+
     def __init__(self, dtype, mu, r_max):
         super(BlendPotentialLayer, self).__init__(dtype=dtype)
         self.mu = tf.constant(mu, dtype=dtype).numpy()
@@ -304,6 +206,24 @@ class BlendPotentialLayer(tf.keras.layers.Layer):
         )
         return config
 
+class PointMassLayer(tf.keras.layers.Layer):
+    def __init__(self, dtype, mu, r_max):
+        super(PointMassLayer, self).__init__(dtype=dtype)
+        self.mu = tf.constant(mu, dtype=dtype).numpy()
+
+    def call(self, inputs):
+        r = tf.linalg.norm(inputs, axis=1, keepdims=True)
+        u_pm = tf.negative(tf.divide(self.mu,r))
+        return u_pm
+
+    def get_config(self):
+        config = super().get_config().copy()
+        config.update(
+            {
+                "mu" : self.mu,
+            }
+        )
+        return config
 
 class PinesAlgorithmLayer(tf.keras.layers.Layer):
     def __init__(self, dtype, mu, a, cBar, sBar):
