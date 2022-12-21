a148d764246fc3d6f98e8be50cb3d1681c147e11
John M <john.r.martin@colorado.edu>
2022-12-21 11:39:12 -0700

    Improvements to nn brillouin map

--- Scripts/Figures/Earth/nn_brillouin_map.py
+++ Scripts/Figures/Earth/nn_brillouin_map.py
@@ -19,22 +19,20 @@ def main():
     directory = os.path.abspath('.') +"/Plots/"
     os.makedirs(directory, exist_ok=True)
 
-    map_vis = MapVisualization('mGal')
+    map_vis = MapVisualization('m/s^2')
     map_vis.fig_size = map_vis.full_page
-    #map_vis.tick_interval = [60, 60]
+    map_vis.tick_interval = [60, 60]
 
-    my_cmap = 'viridis'
     # vlim= [0, 30]
     vlim = None
 
     planet = Earth()
-    model_file = planet.sh_file
     density_deg = 180
 
     df_file ='C:\\Users\\John\\Documents\\Research\\ML_Gravity\\Data\\Dataframes\\pinn_df.data'
     df_file ='Data/Dataframes/earth_trajectory_v2.data'
     df_file ='Data/Dataframes/useless_072621_v2.data'
-    df_file ='Data/Dataframes/earth_pinn_III_v2.data'
+    df_file ='Data/Dataframes/test.data'
 
     df = pd.read_pickle(df_file)
 
@@ -42,32 +40,54 @@ def main():
     # trajectory = DHGridDist(planet, planet.radius+420000, degree=density_deg)
     grav_model = SphericalHarmonics(planet.sh_file,1000,trajectory).load()
     a_1000 = grav_model.accelerations
-    grid_1000 = Grid(trajectory=trajectory, accelerations=a_1000)
+    grid_true = Grid(trajectory=trajectory, accelerations=a_1000)
+
+    grav_model = SphericalHarmonics(planet.sh_file,55,trajectory).load()
+    a_sh = grav_model.accelerations
+    grid_sh = Grid(trajectory=trajectory, accelerations=a_sh)
 
     grav_model = SphericalHarmonics(planet.sh_file,2,trajectory).load()
     a_2 = grav_model.accelerations
-    grid_true = grid_1000 - Grid(trajectory=trajectory, accelerations=a_2)
+    grid_a2 = Grid(trajectory=trajectory, accelerations=a_2)
 
-    # for i in range(len(df)):
-    # with -1 as i:
+
     i = -1
     row = df.iloc[i]
     model_id = row['id']
     config, model = load_config_and_model(model_id, df)
 
+    # The PINN Model
     a_pred = model.compute_acceleration(trajectory.positions)
-    grid_pred = Grid(trajectory=trajectory, accelerations=a_pred)
+    grid_pred = Grid(trajectory=trajectory, accelerations=a_pred) 
 
-    # The learned PINN model 
-    map_vis.plot_grid(grid_pred.total, vlim=vlim, label=None)#"U_{1000}^{(2)} - U_{100}^{(2)}")
-    #map_vis.save(plt.gcf(), directory + "pinn_brillouin_" + str(row['num_units']) + ".pdf")
 
-    # The True Model
-    map_vis.plot_grid(grid_true.total, vlim=vlim, label=None)#"U_{1000}^{(2)} - U_{100}^{(2)}")
+    # If the PINN model includes point mass + J2, remove it
+    if config['deg_removed'][0] == -1:
+        grid_pred -= grid_a2
+        grid_true -= grid_a2
+        grid_sh -= grid_a2
+        
+    vlim = [grid_pred.total.min(), np.mean(grid_pred.total) + 2*np.std(grid_pred.total)]
+    map_vis.plot_grid(grid_pred.total, vlim=vlim, label=None)
+    plt.title("Pred Model a2 no Vlim")
     
-    # Compute percent error across the map
+    vlim = [grid_pred.total.min(), grid_pred.total.max()]
+    map_vis.plot_grid(grid_pred.total, vlim=vlim, label=None)
+    plt.title("Pred Model a2")
+    
+    map_vis.plot_grid(grid_true.total, vlim=vlim, label=None)
+    plt.title("True Model a2")
+
     error = (grid_pred - grid_true)/ grid_true * 100.0
+    map_vis.plot_grid(error.total, vlim=[0,100], label=None)
     print(np.average(error.total))
+    plt.title("Error PINN")
+    
+    error = (grid_sh - grid_true)/ grid_true * 100.0
+    map_vis.plot_grid(error.total, vlim=[0,100], label=None)
+    print(np.average(error.total))
+    plt.title("Error SH")
+
 
     plt.show()
 if __name__ == "__main__":
