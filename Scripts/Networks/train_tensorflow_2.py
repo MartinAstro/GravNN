b213179d342edfa153a0d1270c1011c9048b3cb3
John M <john.r.martin@colorado.edu>
2022-12-31 20:41:08 -0700

    Clean training script

--- Scripts/Networks/train_tensorflow_2.py
+++ Scripts/Networks/train_tensorflow_2.py
@@ -12,75 +12,91 @@ os.environ["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] ='YES'
 
 def main():
 
-    threads = 6
+    threads = 8
 
     df_file = "Data/Dataframes/multiFF_hparams.data" 
     df_file = "Data/Dataframes/multiFF_followup.data" 
+    df_file = "Data/Dataframes/best_10_followup.data" 
+    df_file = "Data/Dataframes/earth_high_alt.data" 
+    df_file = "Data/Dataframes/earth_high_alt3.data" 
+    df_file = "Data/Dataframes/earth_high_alt4.data" 
+    df_file = "Data/Dataframes/new_hparam_search.data" 
+    df_file = "Data/Dataframes/hparams_ll.data" 
+    df_file = "Data/Dataframes/hparams_ll2.data" 
+    df_file = "Data/Dataframes/test_uniform.data" 
+    df_file = "Data/Dataframes/sigma_search.data" 
+    df_file = "Data/Dataframes/alc.data" 
+    df_file = "Data/Dataframes/fourier_search_2.data" 
+    df_file = "Data/Dataframes/high_altitude_behavior.data" 
+    df_file = "Data/Dataframes/eros_PINN_III_temp.data" 
+    df_file = "Data/Dataframes/eros_PINN_III.data" 
+    df_file = "Data/Dataframes/eros_PINN_III_hparams.data" 
+    df_file = "Data/Dataframes/earth_percent_error_test.data" 
+    df_file = "Data/Dataframes/eros_PINN_II_hparams.data" 
 
-    config = get_default_earth_config()
-    config.update(PINN_III())
+    config = get_default_eros_config()
+    # config = get_default_earth_config()
+    # config = get_default_earth_config()
+    config.update(PINN_II())
     config.update(ReduceLrOnPlateauConfig())
 
+    # import numpy as np
+    # import itertools
+    # FF1 = [2**(i) for i in range(-3, 3)]
+    # FF2 = [2**(i) for i in range(-3, 3)]
+    # sigmas = np.array(list(itertools.product(FF1, FF2)))
+
+
     hparams = {
-    
-        "radius_min" : [Earth().radius*1],
-        "radius_max" : [Earth().radius*1.06],
-        "ref_radius_min" : [Earth().radius*1],
-        "ref_radius_max" : [Earth().radius*1.06],
-        "feature_min" : [1.0],
-        "feature_max" : [1.0 + (Earth().radius*1.06)/Earth().radius],
-        "N_dist": [5000],
-        # "N_train": [2000],
-        "N_train": [1000],
-        "N_val" : [100],
-        "batch_size" : [2**20],
-        # "radius_max" : [Earth().radius*5],
-        # "N_dist": [1100000],
-        # "N_train": [1000000],
-        # "batch_size" : [2**20],
 
-        "radius_max" : [Earth().radius + 420000],
-        "N_dist": [500000],
-        "N_train": [20000],
-        "N_val": [5000], 
-        "batch_size" : [2**20],
 
-        "learning_rate": [0.000005],
+        "batch_size" : [2**20],
         "PINN_constraint_fcn" : ['pinn_a'],
 
-        "activation" : ['tanh'],
+        # "N_dist": [50000],
+        # "N_train": [2000],
+        # "N_val": [500],
+        # "epochs" : [5000],
 
-        "ref_radius_max" : [Earth().radius+420000.0],
-        "ref_radius_min" : [Earth().radius],
 
-        "deg_removed" : [2],  #-1
+        "N_dist": [50000],
+        "N_train": [2**11, 2**12, 2**13, 2**14, 2**15],
+        "epochs" : [2**10, 2**11, 2**12, 2**13, 2**14],
+        "num_units": [10, 20, 40, 80],
+        "N_val": [5000],
+
+
+        "learning_rate": [0.001],
+        # "num_units" : [20],
+
         "remove_point_mass" : [False],
         "jit_compile" : [True],
         "eager" : [False],
+        # "jit_compile" : [False],
+        # "eager" : [True],
+
+        # "loss_fcns" : [['percent']],
+        # "dropout" : [0.1],
+
+
+        # "dropout" 
+        # "activation"
+        # "loss_fcns"
+
+        # Features to Investigate
+        "network_type": ["custom"],
         "loss_sph" : [False],
+        
+        
+        # Investigate Later
+        "uniform_volume" : [False],
+        # "preprocessing"
 
-        "num_units": [10],
-        "fourier_features" : [20],
-        "fourier_sigma" : [
-            [1.0, 2.0],
-            ],
-
-        "network_type": ["custom"],        
-
-        "loss_fcns" : [
-            # ['rms', 'percent'],
-            # ['rms'],
-            ['percent'],
-        ],
-
-        # TBD
+        # To be completed
         "lr_anneal": ['hold'],
         "beta": [0.001],
 
-        # Investigate Later
-        "uniform_volume" : [False],
-        "dropout" : [0.00],
-        "preprocessing" : [['pines', 'r_inv']]#, 'r_normalize', 'r_inv']],
+
 
 
     }
@@ -93,10 +109,28 @@ def main():
 
 
 def run(config):    
+
+    def experiment(config, model, radius_bounds, max_percent):
+        planes_exp = PlanesExperiment(model, config, radius_bounds, 30)
+        planes_exp.run()
+        vis = PlanesVisualizer(planes_exp, save_directory=os.path.abspath(".")+"/Plots/Eros/", halt_formatting=True)
+        vis.plot(percent_max=max_percent)
+
+    def extrapolation_experiment(config, model):
+        extrap_exp = ExtrapolationExperiment(model, config, 500)
+        extrap_exp.run()
+        vis = ExtrapolationVisualizer(extrap_exp)
+        vis.plot_interpolation_percent_error()
+        vis.plot_extrapolation_percent_error()
+
     from GravNN.Networks.Data import DataSet
     from GravNN.Networks.Model import PINNGravityModel
     from GravNN.Networks.utils import configure_tensorflow
     from GravNN.Networks.utils import populate_config_objects
+
+    import time
+    import numpy as np
+    import os
 
     configure_tensorflow(config)
 
@@ -104,15 +138,65 @@ def run(config):
     config = populate_config_objects(config)
     pprint(config)
 
-    from GravNN.GravityModels.SphericalHarmonics import SphericalHarmonics
-    grav_model = SphericalHarmonics(Earth().EGM2008,2)
-    config['cBar'] = [grav_model.C_lm]
-    config['sBar'] = [grav_model.S_lm]
+    # import tensorflow as tf
+    # print(tf.config.list_physical_devices('GPU'))
+    # tf.debugging.set_log_device_placement(True)
+    # tf.config.experimental.set_memory_growth(tf.config.list_physical_devices('GPU')[0], True)
+
+    # from GravNN.GravityModels.SphericalHarmonics import SphericalHarmonics
+    # grav_model = SphericalHarmonics(Earth().EGM2008,2)
+    # config['cBar'] = [grav_model.C_lm]
+    # config['sBar'] = [grav_model.S_lm]
 
     # Get data, network, optimizer, and generate model
     data = DataSet(config)
     model = PINNGravityModel(config)
+    np.random.seed(int(os.getpid() % 13))
+    time.sleep(np.random.uniform(0, 5))#))
+
+    # if transfer
+    # for layer in model.network.layers[:4]:
+    #     layer.trainable = False
+
     history = model.train(data)
+
+    # radius = config['planet'][0].radius
+    # radius_bounds = [-1*radius, 1*radius]
+    # experiment(config, model, radius_bounds, max_percent=10)
+    # radius_bounds = [-10*radius, 10*radius]
+    # experiment(config, model, radius_bounds, max_percent=10)
+    # radius_bounds = [-100*radius, 100*radius]
+    # experiment(config, model, radius_bounds, max_percent=10)
+    # extrapolation_experiment(config, model)
+
+
+    # fine tuning
+    if 'fine_tuning_epochs' in config:
+        config['epochs'] = [config['fine_tuning_epochs'][0]]
+        model.set_training(False) # remove dropout 
+        history = model.train(data, initialize_optimizer=False)
+
+    # radius = config['planet'][0].radius
+    # radius_bounds = [-1*radius, 1*radius]
+    # experiment(config, model, radius_bounds, max_percent=10)
+    # radius_bounds = [-10*radius, 10*radius]
+    # experiment(config, model, radius_bounds, max_percent=10)
+    # radius_bounds = [-100*radius, 100*radius]
+    # experiment(config, model, radius_bounds, max_percent=10)
+    # extrapolation_experiment(config, model)
+
+    # import matplotlib.pyplot as plt
+    # # plt.figure()
+    # # plt.plot(history.history['loss'])
+    # # plt.plot(history.history['val_loss'])
+    # # plt.figure()
+    # # plt.plot(history.history['percent_mean'])
+    # # plt.plot(history.history['val_percent_mean'])
+    # plt.show()
+    # plt.close()
+
+    # vis.save(plt.gcf(), "Eros_Planes.pdf")
+
 
     model.save(df_file=None, history=history, transformers=data.transformers)
     return model.config
