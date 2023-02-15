5706ef60ea5dd81ea25a399b18781622c2867b62
John M <john.r.martin@colorado.edu>
2023-02-14 17:53:30 -0700

    Time model scripts and experiment

--- GravNN/Analysis/TimePredictionExperiment.py
+++ GravNN/Analysis/TimePredictionExperiment.py
@@ -1,6 +1,5 @@
-import timeit
+import time
 import numpy as np
-
 class TimePredictionExperiment:
     def __init__(self, model, r_max, r_min, points):
         self.model = model
@@ -15,11 +14,34 @@ class TimePredictionExperiment:
         x_test = random_unit * random_radius
         return x_test
 
-    def time_predictions(self, x):
+    def time_predictions(self, x, batch):
         # warm start
+        try:
+            x = x.astype(self.model.dtype)
+        except:
+            x = x.astype(np.float32)
+        import tensorflow as tf
+        x_tensor = x
+        # x_tensor = tf.data.Dataset.from_tensor_slices(x)
+        self.model.compute_acceleration(x_tensor[0:1])
+        self.model.compute_potential(x_tensor[0:1]) 
         self.model.compute_acceleration(x[0:1]).astype(float)
+        if batch:
+            def eval(x):
+                self.model.compute_acceleration(x_tensor)
+                # self.model.compute_potential(x_tensor)
+        else:
+            def eval(x):
+                for i in range(len(x)):
+                    self.model.compute_acceleration(x_tensor[i:i+1]) 
+                    # self.model.compute_potential(x_tensor[i:i+1]) 
 
         # time calls
+        start = time.time()
+        eval(x)
+        dt = time.time() - start
+
+        print(f"Time per sample {dt / len(x)}")
         timer = timeit.Timer(
             stmt="self.model.compute_acceleration(x)", globals={"self": self, "x": x}
         )
@@ -28,10 +50,12 @@ class TimePredictionExperiment:
         times = np.array(timer.repeat(repeat=5, number=number)) / number
         print(times)
         return times
+        return dt / len(x)
 
-    def run(self):
+    def run(self, batch):
         x = self.get_test_data()
-        self.time_predictions(x)
+        self.times = self.time_predictions(x, batch)
+        return self.times
 
 
 def main():
@@ -42,20 +66,25 @@ def main():
     from GravNN.GravityModels.SphericalHarmonics import SphericalHarmonics
     from GravNN.Networks.Model import load_config_and_model
 
-    df = pd.read_pickle("Data/Dataframes/network_size_test.data")
+    df = pd.read_pickle("Data/Dataframes/example.data")
+    # df = pd.read_pickle("Data/Dataframes/network_size_test.data")
     model_id = df["id"].values[-1]
-    config, model = load_config_and_model(model_id, df)
+    config, pinn_model = load_config_and_model(model_id, df)
     print(f"Number of Units: {config['num_units'][0]}")
     print(f"Number of Params: {config['params'][0]}")
     r_max = config["radius_max"][0]
     r_min = config["radius_min"][0]
 
-    extrapolation_exp = TimePredictionExperiment(model, r_max, r_min, 50)
-    extrapolation_exp.run()
+    # Batch vs Single
+    # Numba vs No Numba
+    # CPU vs GPU
 
-    model = SphericalHarmonics(config["grav_file"][0], degree=100, parallel=True)
-    extrapolation_exp = TimePredictionExperiment(model, r_max, r_min, 50)
-    extrapolation_exp.run()
+    extrapolation_exp = TimePredictionExperiment(pinn_model, r_max, r_min, 1)
+    extrapolation_exp.run(batch=False)
+
+    sh_model = SphericalHarmonics(config["grav_file"][0], degree=100, parallel=False)
+    extrapolation_exp = TimePredictionExperiment(sh_model, r_max, r_min, 100)
+    extrapolation_exp.run(batch=False)
 
 
 if __name__ == "__main__":
