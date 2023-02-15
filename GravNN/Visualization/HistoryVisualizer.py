3fe2fd7f76c4356b9f780750e29820b52fbea19b
John <joma5012@colorado.edu>
2023-02-14 18:06:37 -0700

    Improve history visualizer

--- GravNN/Visualization/HistoryVisualizer.py
+++ GravNN/Visualization/HistoryVisualizer.py
@@ -18,16 +18,16 @@ class HistoryVisualizer(VisualizationBase):
 
     def get_history(self):
         try:
-            self.history = get_history(config['id'][0])
+            self.history = get_history(self.config['id'][0])
         except:
-            self.history = config['history'][0]
+            self.history = self.config['history'][0]
         self.loss = self.history['loss']
         self.val_loss = self.history['val_loss']
         self.percent_error = self.history['percent_mean']
         self.val_percent_error = self.history['val_percent_mean']
         self.epochs = np.arange(0, len(self.loss), 1)
 
-    def plot(self, x, y, label=None, log_x=False, log_y=False):
+    def plot(self, x, y, log_x=False, log_y=False, **kwargs):
         if log_x and log_y:
             plt_fcn = plt.loglog
         elif log_x:
@@ -36,20 +36,40 @@ class HistoryVisualizer(VisualizationBase):
             plt_fcn = plt.semilogy
         else:
             plt_fcn = plt.plot
-        plt_fcn(x, y, label=label)
+        label = kwargs.get('label', "loss")
+        linestyle= kwargs.get('linestyle', '-')
+        color = kwargs.get('color', None)
+        alpha = kwargs.get('alpha', 1.0)
+        line = plt_fcn(x, y, 
+                label=label, 
+                linestyle=linestyle, 
+                color=color,
+                alpha=alpha)
         plt.grid()
         plt.tight_layout()
+        return line
 
     def plot_loss(self, **kwargs):
-        plt.figure()
-        self.plot(self.epochs, self.loss, label='loss', **kwargs)
-        self.plot(self.epochs, self.val_loss, label='val loss', **kwargs)
+        if kwargs.get('new_fig', True):
+            plt.figure()
+        # Plot loss
+        line = self.plot(self.epochs, self.loss, **kwargs)
+        
+        # update validation keywords
+        kwargs.update({"label" : "val " + kwargs.get('label', "loss")})
+
+        # plot validation
+        self.plot(self.epochs, self.val_loss, 
+                    linestyle='--',
+                    alpha=0.5, 
+                    color=line[0].get_color(),
+                    **kwargs)
         skip_epochs = kwargs.get("skip_epochs", 20)
         plt.ylim([None, self.val_loss[skip_epochs]])
         plt.legend()
 
 if __name__ == "__main__":
-    df, idx = pd.read_pickle('Data/Dataframes/example.data'), -1 #i = 105 is best
+    df, idx = pd.read_pickle('Data/Dataframes/big_net_run.data'), -1 #i = 105 is best
 
     model_id = df["id"].values[idx]
     config, model = load_config_and_model(model_id, df)
