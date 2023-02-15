ce5b30c9aefc9e851ded732cda52ef74a5cb19cd
John M <john.r.martin@colorado.edu>
2023-02-14 17:53:42 -0700

    plot to visualize speed

--- Scripts/Figures/model_speed_plot.py
+++ Scripts/Figures/model_speed_plot.py
@@ -7,27 +7,28 @@ from GravNN.Visualization.VisualizationBase import VisualizationBase
 
 def main():
     vis = VisualizationBase(save_directory=os.path.abspath('.') +"/Plots/")
-    vis.fig_size = vis.full_page
-    df = pd.read_pickle('Data/Dataframes/speed_results_journal.data')
-    #df = pd.read_pickle('Data/speed_results_v2.data')
+    description = "no_jit"
+    description = "no_jit_batch"
+
+    pinn_df = pd.read_pickle(f"Data/Dataframes/pinn_model_times_{description}.data")
+    sh_df = pd.read_pickle(f"Data/Dataframes/sh_model_times_{description}.data")
+
+    pinn_df.sort_values(by='params', inplace=True)
+    sh_df.sort_values(by='params', inplace=True)
+
 
     fig, ax = vis.newFig()
-    plt.loglog(df.index, df['poly_time'], label='Poly')
-    plt.loglog(df.index, df['sh_time'], label='SH')
-    plt.loglog(df.index, df['nn_time'], label='NN (GPU)')
-    plt.loglog(df.index, df['pinn_time'], label='PINN (GPU)')
+    plt.plot(pinn_df.params, pinn_df['time [s]'], label='PINN')
+    plt.plot(sh_df.params, sh_df['time [s]'], label='SH')
+    plt.gca().set_xscale('log', base=2)
+    plt.gca().set_yscale('log', base=2)
 
-    try: 
-        plt.loglog(df.index, df['nn_cpu_time'], label='NN (CPU)')
-        plt.loglog(df.index, df['pinn_cpu_time'], label='PINN (CPU)')
-    except:
-        pass
     plt.legend()
 
     plt.xlabel('Parameters')
     plt.ylabel('Time [s]')
 
-    vis.save(fig, 'OneOff/speed_plot.pdf')
+    # vis.save(fig, 'OneOff/speed_plot.pdf')
     plt.show()
 if __name__ == '__main__':
     main()
