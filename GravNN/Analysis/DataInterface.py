627afe5b2b00acbd4a23ba3beb5a789a1d300e83
John <joma5012@colorado.edu>
2023-02-17 11:08:20 -0700

    Add a data interface for experiments
    Helps with retrieving true and predicted data for exp

--- GravNN/Analysis/DataInterface.py
+++ GravNN/Analysis/DataInterface.py
@@ -7,7 +7,7 @@ from GravNN.Networks.Losses import *
 from pprint import pprint
 import matplotlib.pyplot as plt
 
-class PredictionDistributionExperiment:
+class DataInterface:
     def __init__(self, model, config, points, radius_bounds=None, random_seed=1234, remove_J2=False):
         self.config = config.copy()
         self.model = model
@@ -79,50 +79,7 @@ class PredictionDistributionExperiment:
             self.LF_potentials = np.zeros_like(x_unscaled[:,0:1])
         
 
-    def run(self):
+    def gather_data(self):
         self.get_data()
         self.get_PINN_data()
         self.get_J2_data()
-        self.plot()
-
-    def plot(self):
-        plt.figure()
-        da = self.accelerations - self.predicted_accelerations
-        plt.subplot(3,1,1)
-        plt.hist(da[:,0], 250)
-        plt.subplot(3,1,2)
-        plt.hist(da[:,1], 250)
-        plt.subplot(3,1,3)
-        plt.hist(da[:,2], 250)
-        plt.xlabel("Acceleration")
-
-        from GravNN.Support.transformations import project_acceleration
-        da_hill = project_acceleration(self.positions, da)
-        plt.figure()
-        plt.subplot(3,1,1)
-        plt.hist(da_hill[:,0], 250)
-        plt.subplot(3,1,2)
-        plt.hist(da_hill[:,1], 250)
-        plt.subplot(3,1,3)
-        plt.hist(da_hill[:,2], 250)
-        plt.xlabel("Sphere Acceleration")
-
-        plt.figure()
-        plt.hist(self.potentials - self.predicted_potentials, 1000)
-        plt.xlabel("Potentials")
-        plt.show()
-
-
-
-def main():
-    from GravNN.Networks.Model import load_config_and_model
-    df_file = "Data/Dataframes/example.data"
-    df = pd.read_pickle(df_file)
-    model_id = df.iloc[-1]["id"]
-    config, model = load_config_and_model(model_id, df)
-    exp = PredictionDistributionExperiment(model, config, 50000, remove_J2=True)
-    exp.run()
-
-    # plt.show()
-if __name__ == "__main__":
-    main()
