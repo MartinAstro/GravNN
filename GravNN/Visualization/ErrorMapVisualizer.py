e4b595f2a95e5b3a5f96a8bc4b5d9bb32e6cca31
John M <john.r.martin@colorado.edu>
2023-02-21 10:29:32 -0700

    change map visualization to class

---
+++ GravNN/Visualization/ErrorMapVisualizer.py
@@ -0,0 +1,100 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from GravNN.GravityModels.SphericalHarmonics import SphericalHarmonics
+from GravNN.Trajectories import DHGridDist
+from GravNN.Support.Grid import Grid
+from GravNN.Visualization.MapVisualization import MapVisualization
+from GravNN.Networks.Model import load_config_and_model
+
+class ErrorMapVisualizer(MapVisualization):
+    def __init__(self, config, model, sh_deg):
+        super().__init__('m/s^2')
+        self.config = config
+        self.model = model
+        self.sh_deg = sh_deg
+        self.fig_size = self.half_page_default
+        self.tick_interval = [60, 60]
+
+    def plot_models(self, grid_true, grid_pinn, grid_sh):
+        plt.figure(figsize=(8,12))
+
+        plt.subplot(3,1,1)
+        vlim = [grid_pinn.total.min(), np.mean(grid_pinn.total) + 2*np.std(grid_pinn.total)]
+        self.plot_grid(grid_true.total, vlim=vlim, label=None, new_fig=False)
+        plt.gcf().get_axes()[-2].set_title("True Model")
+        
+        plt.subplot(3,1,2)
+        self.plot_grid(grid_pinn.total, vlim=vlim, label=None, new_fig=False)
+        plt.gcf().get_axes()[-2].set_title("PINN Model")
+        
+        plt.subplot(3,1,3)
+        self.plot_grid(grid_sh.total, vlim=vlim, label=None, new_fig=False)
+        plt.gcf().get_axes()[-2].set_title("SH Model")
+
+    def plot_percent_error(self, sh_percent, pinn_percent):
+        vlim = [0, 100]
+        plt.figure(figsize=(8,12))
+
+        plt.subplot(2,1,1)
+        self.plot_grid(pinn_percent.total, vlim=vlim, label=None, new_fig=False)
+        plt.gcf().get_axes()[-2].set_title(f"PINN Percent Error: {np.average(pinn_percent.total)}")
+        
+        plt.subplot(2,1,2)
+        self.plot_grid(sh_percent.total, vlim=vlim, label=None, new_fig=False)
+        plt.gcf().get_axes()[-2].set_title(f"SH Percent Error: {np.average(sh_percent.total)}")
+
+    def plot_rms(self, sh_diff, pinn_diff):
+        vlim = [pinn_diff.total.min(), np.mean(pinn_diff.total) + 2*np.std(pinn_diff.total)]
+        plt.figure(figsize=(8,12))
+
+        plt.subplot(2,1,1)
+        self.plot_grid(pinn_diff.total, vlim=vlim, label=None, new_fig=False)
+        plt.gcf().get_axes()[-2].set_title(f"PINN RMS Diff: {np.average(pinn_diff.total)}")
+        
+        plt.subplot(2,1,2)
+        self.plot_grid(sh_diff.total, vlim=vlim, label=None, new_fig=False)
+        plt.gcf().get_axes()[-2].set_title(f"SH RMS Diff: {np.average(sh_diff.total)}")
+
+
+
+    def plot(self, trajectory):
+        planet = self.config['planet'][0]
+
+        # True Model
+        grav_model = SphericalHarmonics(planet.sh_file,1000,trajectory).load()
+        a_1000 = grav_model.accelerations
+        grid_true = Grid(trajectory=trajectory, accelerations=a_1000)
+
+        # Low fidelity SH Model
+        grav_model = SphericalHarmonics(planet.sh_file,self.sh_deg,trajectory).load()
+        a_sh = grav_model.accelerations
+        grid_sh = Grid(trajectory=trajectory, accelerations=a_sh)
+
+        # Planetary oblateness model
+        grav_model = SphericalHarmonics(planet.sh_file,2,trajectory).load()
+        a_2 = grav_model.accelerations
+        grid_a2 = Grid(trajectory=trajectory, accelerations=a_2)
+
+        # The PINN Model
+        a_pinn = self.model._compute_acceleration(trajectory.positions)
+        grid_pinn = Grid(trajectory=trajectory, accelerations=a_pinn) 
+
+
+        # If the PINN model includes point mass + J2, remove it
+        if self.config['deg_removed'][0] == -1:
+            grid_pinn -= grid_a2
+        grid_true -= grid_a2
+        grid_sh -= grid_a2
+
+        pinn_diff = grid_pinn - grid_true
+        sh_diff = grid_sh - grid_true 
+
+        pinn_percent = pinn_diff / grid_pinn * 100
+        sh_percent = sh_diff / grid_sh * 100
+
+        self.plot_models(grid_true, grid_pinn, grid_sh)
+        self.plot_percent_error(sh_percent, pinn_percent)
+        self.plot_rms(sh_diff, pinn_diff)
+    
--- Scripts/Figures/Earth/nn_brillouin_map.py
+++ Scripts/Figures/Earth/nn_brillouin_map.py
@@ -1,96 +1,11 @@
-        
 import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from GravNN.CelestialBodies.Planets import Earth
-from GravNN.GravityModels.SphericalHarmonics import SphericalHarmonics
 from GravNN.Trajectories import DHGridDist
-from GravNN.Support.Grid import Grid
-from GravNN.Visualization.MapVisualization import MapVisualization
+from GravNN.Visualization.ErrorMapVisualizer import ErrorMapVisualizer
 from GravNN.Networks.Model import load_config_and_model
-
-def plot(df, idx, planet, trajectory):
-
-    map_vis = MapVisualization('m/s^2')
-    map_vis.fig_size = map_vis.full_page_default
-    map_vis.tick_interval = [60, 60]
-
-    grav_model = SphericalHarmonics(planet.sh_file,1000,trajectory).load()
-    a_1000 = grav_model.accelerations
-    grid_true = Grid(trajectory=trajectory, accelerations=a_1000)
-
-    grav_model = SphericalHarmonics(planet.sh_file,55,trajectory).load()
-    a_sh = grav_model.accelerations
-    grid_sh = Grid(trajectory=trajectory, accelerations=a_sh)
-
-    grav_model = SphericalHarmonics(planet.sh_file,2,trajectory).load()
-    a_2 = grav_model.accelerations
-    grid_a2 = Grid(trajectory=trajectory, accelerations=a_2)
-
-    row = df.iloc[idx]
-    model_id = row['id']
-    config, model = load_config_and_model(model_id, df)
-
-    # The PINN Model
-    a_pred = model._compute_acceleration(trajectory.positions)
-    grid_pred = Grid(trajectory=trajectory, accelerations=a_pred) 
-
-    # If the PINN model includes point mass + J2, remove it
-    if config['deg_removed'][0] == -1:
-        grid_pred -= grid_a2
-    grid_true -= grid_a2
-    grid_sh -= grid_a2
-        
-
-    plt.figure(figsize=(8,6))
-    plt.subplot(2,1,1)
-    vlim = [grid_pred.total.min(), np.mean(grid_pred.total) + 2*np.std(grid_pred.total)]
-    map_vis.plot_grid(grid_pred.total, vlim=vlim, label=None, new_fig=False)
-    plt.gcf().get_axes()[-2].set_title("PINN Model no Vlim")
-    
-    plt.subplot(2,1,2)
-    map_vis.plot_grid(grid_true.total, vlim=vlim, label=None, new_fig=False)
-    plt.gcf().get_axes()[-2].set_title("True Model")
-    
-
-    plt.figure(figsize=(8,6))
-    plt.subplot(2,2,1)
-    vlim = [grid_pred.total.min(), grid_pred.total.max()]
-    map_vis.plot_grid(grid_pred.total, vlim=vlim, label=None, new_fig=False)
-    plt.gcf().get_axes()[-2].set_title("PINN Model a2")
-    
-    plt.subplot(2,2,3)
-    map_vis.plot_grid(grid_sh.total, vlim=vlim, label=None, new_fig=False)
-    plt.gcf().get_axes()[-2].set_title("SH Model a2")
-
-    plt.subplot(2,2,2)
-    PINN_grid_difference = grid_pred - grid_true
-    map_vis.plot_grid(PINN_grid_difference.total, vlim=vlim, label=None, new_fig=False)
-    plt.gcf().get_axes()[-2].set_title("PINN Model Diff")
-    
-    plt.subplot(2,2,4)
-    SH_grid_difference = grid_sh - grid_true
-    map_vis.plot_grid(SH_grid_difference.total, vlim=vlim, label=None, new_fig=False)
-    plt.gcf().get_axes()[-2].set_title("SH Model Diff")
-
-
-
-    plt.figure(figsize=(8,6))
-    plt.subplot(2,1,1)
-    error = PINN_grid_difference / grid_true * 100.0
-    map_vis.plot_grid(error.total, vlim=[0,100], label=None, new_fig=False)
-    print(f"PINN Percent Error: {np.average(error.total)}")
-    print(f"PINN RMS Error: {np.average(PINN_grid_difference.total)}")
-    plt.gcf().get_axes()[-2].set_title("Error PINN")
-
-    plt.subplot(2,1,2)
-    error = SH_grid_difference / grid_true * 100.0
-    map_vis.plot_grid(error.total, vlim=[0,100], label=None, new_fig=False)
-    print(f"SH Percent Error: {np.average(error.total)}")
-    print(f"SH RMS Error: {np.average(SH_grid_difference.total)}")
-    plt.gcf().get_axes()[-2].set_title("Error SH")
-
 
 def main():
     # Plotting 
@@ -103,19 +18,32 @@ def main():
     # density_deg = 25 # 5000
     df_file, idx = "Data/Dataframes/example.data", -1
 
-
     df = pd.read_pickle(df_file)
+    model_id = df['id'].iloc[idx]
+    config, model = load_config_and_model(model_id, df)
+
+    # plt.switch_backend("WebAgg")
+    vis = ErrorMapVisualizer(config, model, sh_deg=55)
 
     surface_data = DHGridDist(planet, planet.radius, degree=density_deg)
-    plot(df, idx, planet, surface_data)
+    vis.plot(surface_data)
     
+<<<<<<< HEAD
     # LEO_data = DHGridDist(planet, planet.radius+420000, degree=density_deg)
     # plot(df, idx, planet, LEO_data)
     
     # high_alt_data = DHGridDist(planet, planet.radius*10, degree=density_deg)
     # plot(df, idx, planet, high_alt_data)
 
+=======
+    LEO_data = DHGridDist(planet, planet.radius+420000, degree=density_deg)
+    vis.plot(LEO_data)
+    
+    # high_alt_data = DHGridDist(planet, planet.radius*10, degree=density_deg)
+    # plot(config, model, high_alt_data, sh_deg)
+>>>>>>> a53738c (change map visualization to class)
 
+    plt.show()
     # plt.show()
 if __name__ == "__main__":
     main()
