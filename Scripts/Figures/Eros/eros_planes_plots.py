b87717cf6fd4d1307cb1cb49d5716f1e16606fa9
John M <john.r.martin@colorado.edu>
2023-03-15 10:56:31 -0600

    Clean planes experiment

--- Scripts/Figures/Eros/eros_planes_plots.py
+++ Scripts/Figures/Eros/eros_planes_plots.py
@@ -7,6 +7,7 @@ from GravNN.CelestialBodies.Asteroids import Eros
 
 import matplotlib.pyplot as plt
 from GravNN.Networks.Model import load_config_and_model
+from GravNN.GravityModels.Polyhedral import get_poly_data
 
 def SRP(A2M, Cr, P_srp, r_body_2_sun):
     a_SRP = -Cr*A2M*P_srp/r_body_2_sun**2
@@ -14,33 +15,22 @@ def SRP(A2M, Cr, P_srp, r_body_2_sun):
 
 
 def main():
-    # df = pd.read_pickle("Data/Dataframes/eros_pinn_III_040622.data")
-    # model_id = df["id"].values[4] # ALC without extra
-    # radius_bounds = [-planet.radius*500, planet.radius*500]
-    # max_percent = 20
-
-    # df = pd.read_pickle("Data/Dataframes/eros_residual_r_bar_trans_alc.data")
-    # df = pd.read_pickle("Data/Dataframes/eros_pinn_II_III_warm_start.data")
-    df = pd.read_pickle("Data/Dataframes/eros_comp_planes.data")
-    
-    model_id = df["id"].values[0] # ALC without extra
-    model_id = df["id"].values[-1] # ALC without extra
 
 
+    df = pd.read_pickle("Data/Dataframes/eros_point_mass_gen_III.data")
 
-    # df = pd.read_pickle("Data/Dataframes/eros_residual_r.data")
+    model_id = df["id"].values[-1] 
 
     print(model_id)
     config, model = load_config_and_model(model_id, df)
     config['grav_file'] = [Eros().obj_8k]
 
     planet = config['planet'][0]
+    config['gravity_data_fcn'] = [get_poly_data]
+    radius_bounds = [-planet.radius*3, planet.radius*3]
+    max_percent = 10
 
-    
-    radius_bounds = [-planet.radius*1, planet.radius*1]
-    max_percent = 1
 
-    # planes_exp = PlanesExperiment(model, config, [-planet.radius*1, planet.radius*1], 30)
     planes_exp = PlanesExperiment(model, config, radius_bounds, 30)
     planes_exp.run()
     vis = PlanesVisualizer(planes_exp, save_directory=os.path.abspath(".")+"/Plots/Eros/")
@@ -58,8 +48,7 @@ def main():
     vis.set_SRP_contour(a_srp)
 
     vis.plot(percent_max=max_percent)
-    vis.save(plt.gcf(), "Eros_Planes.pdf")
-    # vis.plot_RMS()
+    # vis.save(plt.gcf(), "Eros_Planes.pdf")
 
     plt.show()
 
