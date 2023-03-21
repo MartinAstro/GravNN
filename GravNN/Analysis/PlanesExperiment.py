20f63bead634beafc66344b938176d8be2bc2ee4
John M <john.r.martin@colorado.edu>
2023-03-21 14:38:44 -0600

    Format planes experiment

--- GravNN/Analysis/PlanesExperiment.py
+++ GravNN/Analysis/PlanesExperiment.py
@@ -17,9 +17,9 @@ class PlanesExperiment:
         self.samples_1d = samples_1d
         self.interior_mask = None
 
-        self.brillouin_radius = config['planet'][0].radius
-        original_max_radius = self.config['radius_max'][0]
-        extra_max_radius = self.config.get('extra_radius_max', [0])[0]
+        self.brillouin_radius = config["planet"][0].radius
+        original_max_radius = self.config["radius_max"][0]
+        extra_max_radius = self.config.get("extra_radius_max", [0])[0]
         max_radius = np.max([original_max_radius, extra_max_radius])
         self.training_bounds = np.array([-max_radius, max_radius])
 
@@ -32,27 +32,27 @@ class PlanesExperiment:
 
         self.percent_error_acc = None
         self.percent_error_pot = None
-        self.loss_fcn_list = ['rms', 'percent', 'angle', 'magnitude']
-
+        self.loss_fcn_list = ["rms", "percent", "angle", "magnitude"]
 
     def get_train_data(self):
         data = DataSet(self.config)
-        self.x_train = data.raw_data['x_train']
-        self.a_train = data.raw_data['a_train']
-        
+        self.x_train = data.raw_data["x_train"]
+        self.a_train = data.raw_data["a_train"]
+
     def get_test_data(self):
-        planet = self.config['planet'][0]
-        obj_file = self.config.get('grav_file',[None])[0]
-        gravity_data_fcn = self.config['gravity_data_fcn'][0]
-        interpolation_dist = PlanesDist(planet, 
-                            bounds=self.bounds,
-                            samples_1d=self.samples_1d,
-                            **self.config,
-                            )
+        planet = self.config["planet"][0]
+        obj_file = self.config.get("grav_file", [None])[0]
+        gravity_data_fcn = self.config["gravity_data_fcn"][0]
+        interpolation_dist = PlanesDist(
+            planet,
+            bounds=self.bounds,
+            samples_1d=self.samples_1d,
+            **self.config,
+        )
 
         full_dist = interpolation_dist
-        
-        x, a, u = gravity_data_fcn(full_dist, obj_file, **self.config)        
+
+        x, a, u = gravity_data_fcn(full_dist, obj_file, **self.config)
 
         self.x_test = x
         self.a_test = a
@@ -61,26 +61,29 @@ class PlanesExperiment:
     def get_model_data(self):
         dtype = self.model.network.compute_dtype
         positions = self.x_test.astype(dtype)
-        self.a_pred = tf.constant(self.model.compute_acceleration(positions), dtype=dtype)
+        self.a_pred = tf.constant(
+            self.model.compute_acceleration(positions),
+            dtype=dtype,
+        )
         self.u_pred = tf.constant(self.model.compute_potential(positions), dtype)
 
         self.a_pred = self.a_pred.numpy().astype(float)
         self.u_pred = self.u_pred.numpy().astype(float)
-        
+
     def compute_percent_error(self):
         def percent_error(x_hat, x_true):
             diff_mag = np.linalg.norm(x_true - x_hat, axis=1)
             true_mag = np.linalg.norm(x_true, axis=1)
-            percent_error = diff_mag/true_mag*100
+            percent_error = diff_mag / true_mag * 100
             return percent_error
-        
+
         self.percent_error_acc = percent_error(self.a_pred, self.a_test)
         self.percent_error_pot = percent_error(self.u_pred, self.u_test)
 
     def compute_RMS(self):
         def RMS(x_hat, x_true):
             return np.sqrt(np.sum(np.square(x_true - x_hat), axis=1))
-        
+
         self.RMS_acc = RMS(self.a_pred, self.a_test)
         self.RMS_pot = RMS(self.u_pred, self.u_test)
 
@@ -88,29 +91,35 @@ class PlanesExperiment:
         losses = {}
         for loss_key in loss_fcn_list:
             loss_fcn = get_loss_fcn(loss_key)
-            
+
             # Compute loss on acceleration and potential
-            losses.update({
-                f"{loss_fcn.__name__}" : loss_fcn(
-                    self.a_pred, 
-                    self.a_test
-                    ).numpy()
-                })
+            losses.update(
+                {
+                    f"{loss_fcn.__name__}": loss_fcn(
+                        self.a_pred,
+                        self.a_test,
+                    ).numpy(),
+                },
+            )
         self.losses = losses
 
     def get_planet_mask(self):
         # Don't recompute this
         if self.interior_mask is None:
-            grav_file =  self.config.get("grav_file", [None])[0] # asteroids grav_file is the shape model
-            self.model_file = self.config.get("shape_model", [grav_file])[0] # planets have shape model (sphere currently)
+            # asteroids grav_file is the shape model
+            grav_file = self.config.get("grav_file", [None])[0]
+            # planets have shape model (sphere currently)
+            self.model_file = self.config.get("shape_model", [grav_file])[0]
             filename, file_extension = os.path.splitext(self.model_file)
-            self.shape_model = trimesh.load_mesh(self.model_file, file_type=file_extension[1:])
+            self.shape_model = trimesh.load_mesh(
+                self.model_file,
+                file_type=file_extension[1:],
+            )
             distances = self.shape_model.nearest.signed_distance(
-                        self.x_test / 1e3
-                    )
+                self.x_test / 1e3,
+            )
             self.interior_mask = distances > 0
         return self.interior_mask
-
 
     def run(self):
         self.get_train_data()
@@ -125,26 +134,29 @@ def main():
     import matplotlib.pyplot as plt
     import pandas as pd
 
-    from GravNN.CelestialBodies.Asteroids import Eros
     from GravNN.Networks.Model import load_config_and_model
     from GravNN.Visualization.PlanesVisualizer import PlanesVisualizer
+
     df = pd.read_pickle("Data/Dataframes/test.data")
-    model_id = df["id"].values[-1] 
+    model_id = df["id"].values[-1]
     config, model = load_config_and_model(model_id, df)
 
-    planet = config['planet'][0]
+    planet = config["planet"][0]
     planes_exp = PlanesExperiment(model, config, [-planet.radius, planet.radius], 30)
     planes_exp.run()
 
     vis = PlanesVisualizer(planes_exp)
     vis.plot(percent_max=10)
 
-    planes_exp = PlanesExperiment(model, config, [-10*planet.radius, 10*planet.radius], 30)
+    radius_bounds = [-10 * planet.radius, 10 * planet.radius]
+    planes_exp = PlanesExperiment(model, config, radius_bounds, 30)
     planes_exp.run()
 
     vis = PlanesVisualizer(planes_exp)
     vis.plot(percent_max=10)
 
     plt.show()
+
+
 if __name__ == "__main__":
     main()
