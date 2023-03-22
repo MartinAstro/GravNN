d331e93ed4f9ce8018e3b8f167f74708368fcec7
John M <john.r.martin@colorado.edu>
2023-03-22 10:38:13 -0600

    Format annealing

--- GravNN/Networks/Annealing.py
+++ GravNN/Networks/Annealing.py
@@ -1,18 +1,24 @@
 from collections import OrderedDict
+
 import tensorflow as tf
+
 from GravNN.Networks.Losses import MetaLoss
 
+
 def get_annealing_fcn(use_anneal):
-    from GravNN.Networks.Annealing import update_w_loss, hold_constant
+    from GravNN.Networks.Annealing import hold_constant, update_w_loss
+
     if use_anneal:
         return update_w_loss
     else:
         return hold_constant
 
+
 def log10(x):
     numerator = tf.math.log(x)
     denominator = tf.math.log(tf.constant(10, dtype=numerator.dtype))
-    return numerator/denominator
+    return numerator / denominator
+
 
 def compute_loss_subset(y_hat_dict, y_dict, loss_fcn_list):
     batch_size = tf.constant(50, dtype=tf.int64)
@@ -29,28 +35,34 @@ def compute_loss_subset(y_hat_dict, y_dict, loss_fcn_list):
     losses_subset = MetaLoss(y_hat_dict_subset, y_dict_subset, loss_fcn_list)
     return losses_subset
 
+
 def hold_constant(w_loss, train_counter, losses, variables, tape):
     return w_loss
+
 
 # ANNEALING V2
 def update_w_loss(w_loss, train_counter, losses, variables, tape):
     traces = []
-    update_interval = tf.constant(1000, dtype=tf.int64)
+    update_interval = tf.constant(100, dtype=tf.int64)
     min_start_idx = tf.constant(10, dtype=tf.int64)
 
-    if tf.math.mod(train_counter, update_interval) == 0 and \
-        train_counter > min_start_idx:
+    if (
+        tf.math.mod(train_counter, update_interval) == 0
+        and train_counter > min_start_idx
+    ):
         for loss_i in losses.values():
-            jacobian = tape.jacobian(loss_i, variables) 
+            jacobian = tape.jacobian(loss_i, variables)
             gradients = []
-            for i in range(len(jacobian)-1): #batch size
-                gradients.append(tf.reshape(jacobian[i], (len(jacobian[i]),-1))) # flatten
+            for i in range(len(jacobian) - 1):  # batch size
+                gradients.append(
+                    tf.reshape(jacobian[i], (len(jacobian[i]), -1)),
+                )  # flatten
             J = tf.concat(gradients, 1)
-            K_i = J@tf.transpose(J) # NTK of the values  [N_samples x N_samples]
+            K_i = J @ tf.transpose(J)  # NTK of the values  [N_samples x N_samples]
             trace_K_i = tf.linalg.trace(K_i)
             traces.append(trace_K_i)
         trace_K = tf.reduce_sum(traces)
-        w_loss_new = tf.stack([trace_K/trace for trace in traces],0)
+        w_loss_new = tf.stack([trace_K / trace for trace in traces], 0)
         w_loss.assign(w_loss_new)
         tf.print(w_loss)
     return w_loss
