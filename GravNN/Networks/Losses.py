59d8ffa203fed1a4fe5c558e2bc758727e993c0e
John <joma5012@colorado.edu>
2023-03-23 10:38:39 -0600

    Format losses and remove RMS A

--- GravNN/Networks/Losses.py
+++ GravNN/Networks/Losses.py
@@ -1,23 +1,31 @@
-import tensorflow as tf
-import numpy as np
 from collections import OrderedDict
-def get_loss_fcn(loss_key):
-    return { 
-        "rms" : rms,
-        "percent" : percent,
-        "angle" : angle,
-        "magnitude" : magnitude
 
+import numpy as np
+import tensorflow as tf
+
+
+def get_loss_fcn(loss_key):
+    return {
+        "rms": rms,
+        "percent": percent,
+        "angle": angle,
+        "magnitude": magnitude,
     }[loss_key.lower()]
 
-#https://github.com/tensorflow/tensorflow/issues/12071#issuecomment-420279641
+
+# https://github.com/tensorflow/tensorflow/issues/12071#issuecomment-420279641
 @tf.custom_gradient
 def norm(x):
     y = tf.norm(x, axis=1, keepdims=True)
+
     def grad(dy):
-        #dy has inf's sometimes
-        result = tf.math.multiply_no_nan(dy, (x / (y + tf.constant(1e-16, dtype=y.dtype))))
+        # dy has inf's sometimes
+        result = tf.math.multiply_no_nan(
+            dy,
+            (x / (y + tf.constant(1e-16, dtype=y.dtype))),
+        )
         return result
+
     return y, grad
 
 
@@ -25,9 +33,12 @@ def MetaLoss(y_hat_dict, y_dict, loss_fcn_list):
     losses = OrderedDict()
     for loss_fcn in loss_fcn_list:
         for key in y_hat_dict.keys() & y_dict.keys():
-            
             # Don't compute percent of laplacian, curl
-            if loss_fcn.__name__ == 'percent' and (key == 'laplacian' or key == 'curl'):
+            loss_name = loss_fcn.__name__
+            laplacian_or_curl = key == "laplacian" or key == "curl"
+            invalid_percent = loss_name == "percent" and laplacian_or_curl
+            biased_rms = loss_name == "rms" and (key == "acceleration")
+            if invalid_percent or biased_rms:
                 continue
 
             y_hat = y_hat_dict[key]
@@ -36,8 +47,8 @@ def MetaLoss(y_hat_dict, y_dict, loss_fcn_list):
 
             # Don't hold losses of zero
             if tf.math.count_nonzero(loss) != 0:
-                loss_name = f"{key}_{loss_fcn.__name__}" 
-                losses.update({loss_name : loss}) 
+                loss_name = f"{key}_{loss_fcn.__name__}"
+                losses.update({loss_name: loss})
 
     return losses
 
@@ -46,29 +57,31 @@ def rms(y_hat, y):
     dy = y_hat - y
     return tf.sqrt(norm(dy))
 
+
 def percent(y_hat, y):
-    da = tf.subtract(y_hat[:,0:3], y[:,0:3])
+    da = tf.subtract(y_hat[:, 0:3], y[:, 0:3])
     da_norm = norm(da)
-    a_true_norm = norm(y[:,0:3])
-    loss_components = tf.math.divide_no_nan(da_norm,a_true_norm) 
+    a_true_norm = norm(y[:, 0:3])
+    loss_components = tf.math.divide_no_nan(da_norm, a_true_norm)
     return loss_components
 
 
 def angle(y_hat, y):
-    a_hat = y_hat[:,0:3]
-    a_hat_mag = tf.norm(a_hat,axis=1)
+    a_hat = y_hat[:, 0:3]
+    a_hat_mag = tf.norm(a_hat, axis=1)
 
-    a = y[:,0:3]
-    a_mag = tf.norm(a,axis=1)
-    
-    eps = tf.constant(1E-7, dtype=y.dtype)
-    cos_theta = tf.reduce_sum(a_hat*a,axis=1)/(a_hat_mag*a_mag)
-    cos_theta_clipped = tf.clip_by_value(cos_theta, -1.0+eps, 1.0-eps)
+    a = y[:, 0:3]
+    a_mag = tf.norm(a, axis=1)
+
+    eps = tf.constant(1e-7, dtype=y.dtype)
+    cos_theta = tf.reduce_sum(a_hat * a, axis=1) / (a_hat_mag * a_mag)
+    cos_theta_clipped = tf.clip_by_value(cos_theta, -1.0 + eps, 1.0 - eps)
     theta = tf.acos(cos_theta_clipped) / tf.constant(np.pi, dtype=y.dtype)
     return theta
 
+
 def magnitude(y_hat, y):
-    a_hat_mag = tf.norm(y_hat[:,0:3],axis=1)
-    a_mag = tf.norm(y[:,0:3],axis=1)
+    a_hat_mag = tf.norm(y_hat[:, 0:3], axis=1)
+    a_mag = tf.norm(y[:, 0:3], axis=1)
     mag_error = tf.abs(a_hat_mag - a_mag) / a_mag
     return mag_error
