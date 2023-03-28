8fd2a091fec2b9ce91f8e27d4ec9547a1cb285a4
John <joma5012@colorado.edu>
2023-03-28 14:43:18 -0600

    Format Earth Config

--- GravNN/Networks/Configs/Earth_Configs.py
+++ GravNN/Networks/Configs/Earth_Configs.py
@@ -1,11 +1,8 @@
 from GravNN.CelestialBodies.Planets import Earth
-from GravNN.GravityModels.SphericalHarmonics import get_sh_data
+from GravNN.GravityModels.SphericalHarmonics import SphericalHarmonics, get_sh_data
 from GravNN.Trajectories import RandomDist
-from GravNN.Preprocessors import DummyScaler, UniformScaler
-from sklearn.preprocessing import MinMaxScaler
 
-from GravNN.GravityModels.SphericalHarmonics import SphericalHarmonics
-grav_model = SphericalHarmonics(Earth().EGM2008,2)
+grav_model = SphericalHarmonics(Earth().EGM2008, 2)
 
 
 def get_default_earth_config():
@@ -18,17 +15,17 @@ def get_default_earth_config():
         "N_val": [50000],
         "radius_min": [Earth().radius],
         "radius_max": [Earth().radius + 420000.0],
-        "ref_radius" : [Earth().radius],
+        "ref_radius": [Earth().radius],
         "acc_noise": [0.00],
         "basis": [None],
         "deg_removed": [-1],
         "mixed_precision": [False],
         "max_deg": [1000],
         "analytic_truth": ["sh_stats_"],
-        "gravity_data_fcn" : [get_sh_data],
-        "shape_model" : [Earth().shape_model],
-        "mu" : [Earth().mu],
-        "cBar" : [grav_model.C_lm],
-        "sBar" : [grav_model.S_lm]
+        "gravity_data_fcn": [get_sh_data],
+        "shape_model": [Earth().shape_model],
+        "mu": [Earth().mu],
+        "cBar": [grav_model.C_lm],
+        "sBar": [grav_model.S_lm],
     }
     return data_config
