469a275fe6b5a2ddb8600e5de8d4f766623db419
John <joma5012@colorado.edu>
2023-03-28 14:47:43 -0600

    Format analyze_planets

--- Scripts/Networks/analyze_planets.py
+++ Scripts/Networks/analyze_planets.py
@@ -1,58 +1,23 @@
-import os
-os.environ["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] ='YES'
-
 import multiprocessing as mp
+import os
+
 import pandas as pd
+
+from GravNN.Analysis.PlanetAnalyzer import PlanetAnalyzer
 from GravNN.CelestialBodies.Planets import Earth, Moon
-from GravNN.CelestialBodies.Asteroids import Bennu
-from GravNN.Trajectories import DHGridDist, FibonacciDist, RandomDist, SurfaceDist
+from GravNN.Networks.Model import load_config_and_model
 from GravNN.Networks.script_utils import save_analysis
+from GravNN.Trajectories import FibonacciDist
+
+os.environ["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] = "YES"
 
 
-def main():
-    """
-    Multiprocess the analysis pipeline for each network within the dataframe
-    """
-    df_file = "Data/Dataframes/earth_trajectory_v2.data"
-    df_file = 'Data/Dataframes/earth_trajectory_deg2.data'
-    planet = Earth()
-
-    df_file = 'Data/Dataframes/moon_traditional_nn_df.data'
-    # df_file = 'Data/Dataframes/moon_pinn_df.data'
-    planet = Moon()
-    analyze_altitude = True
-    
-    # df_file = "Data/Dataframes/traditional_nn_df_percent.data"
-    # df_file = "Data/Dataframes/pinn_df_percent.data"
-
-
-
-    points = 250000  # 64800
-    #test_trajectories = {"Brillouin_deg0": FibonacciDist(planet, planet.radius, points)}
-    #test_trajectories = {"Brillouin_deg_n1": FibonacciDist(planet, planet.radius, points)}
-    test_trajectories = {"Brillouin": FibonacciDist(planet, planet.radius, points)}
-    # test_trajectories = {"Brillouin_percent": FibonacciDist(planet, planet.radius, points)}
-
-    args = []
-    for idx in range(0,10):
-        args.append((idx, df_file, planet, analyze_altitude, test_trajectories, points))
-
-    with mp.Pool(1) as pool:
-        output = pool.starmap_async(analyze, args)
-        results = output.get()
-
-    save_analysis(df_file, results)
-
-
-def analyze(idx, df_file, planet, analyze_altitude, test_trajectories, points):
+def analyze(idx, df, planet, analyze_altitude, test_trajectories, points):
     from GravNN.Networks.script_utils import get_altitude_list
     from GravNN.Networks.utils import configure_tensorflow
 
     tf = configure_tensorflow()
-    from GravNN.Analysis.PlanetAnalyzer import PlanetAnalyzer
-    from GravNN.Networks.Model import load_config_and_model
 
-    df = pd.read_pickle(df_file)
     if idx >= len(df):
         return (None, None)
 
@@ -69,6 +34,34 @@ def analyze(idx, df_file, planet, analyze_altitude, test_trajectories, points):
 
     print(rse_entries)
     return (model_id, rse_entries)
+
+
+def main():
+    """
+    Multiprocess the analysis pipeline for each network within the dataframe
+    """
+    df_file = "Data/Dataframes/earth_trajectory_v2.data"
+    planet = Earth()
+
+    df_file = "Data/Dataframes/moon_traditional_nn_df.data"
+    planet = Moon()
+
+    threads = 1
+    analyze_altitude = False
+    points = 250000  # 64800
+    test_trajectories = {"Brillouin": FibonacciDist(planet, planet.radius, points)}
+
+    df = pd.read_pickle(df_file)
+
+    args = []
+    for idx in range(0, len(df)):
+        args.append((idx, df, planet, analyze_altitude, test_trajectories, points))
+
+    with mp.Pool(threads) as pool:
+        output = pool.starmap_async(analyze, args)
+        results = output.get()
+
+    save_analysis(df_file, results)
 
 
 if __name__ == "__main__":
