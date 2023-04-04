0b996639f29b7ca275196961971077feda7d4d97
John M <john.r.martin@colorado.edu>
2023-04-04 11:12:28 -0600

    Generate earth data script

---
+++ Scripts/Data/generate_earth_data.py
@@ -0,0 +1,21 @@
+from GravNN.CelestialBodies.Planets import Earth
+from GravNN.GravityModels.SphericalHarmonics import SphericalHarmonics
+from GravNN.Trajectories import RandomDist
+
+
+def main():
+    planet = Earth()
+    model_file = planet.sh_file
+    bounds = [planet.radius, planet.radius + 420000.0]
+    trajectory = RandomDist(planet, bounds, points=5000000)
+    gravity_model = SphericalHarmonics(
+        model_file,
+        degree=1000,
+        trajectory=trajectory,
+        parallel=False,
+    )
+    gravity_model.load()
+
+
+if __name__ == "__main__":
+    main()
