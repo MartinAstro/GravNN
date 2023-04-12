ff9933136174db6a6ce353d3c4f0e75dae6e27e6
John M <john.r.martin@colorado.edu>
2023-04-12 14:15:18 -0600

    Format and path conversion

--- GravNN/Analysis/DataInterface.py
+++ GravNN/Analysis/DataInterface.py
@@ -1,28 +1,34 @@
-from GravNN.Networks.utils import _get_loss_fcn
-from GravNN.Networks.Data import DataSet
-from GravNN.Trajectories.RandomDist import RandomDist
 import numpy as np
-import pandas as pd
+
 from GravNN.Networks.Losses import *
-from pprint import pprint
-import matplotlib.pyplot as plt
+from GravNN.Support.PathTransformations import make_windows_path_posix
+from GravNN.Trajectories.RandomDist import RandomDist
+
 
 class DataInterface:
-    def __init__(self, model, config, points, radius_bounds=None, random_seed=1234, remove_J2=False):
+    def __init__(
+        self,
+        model,
+        config,
+        points,
+        radius_bounds=None,
+        random_seed=1234,
+        remove_J2=False,
+    ):
         self.config = config.copy()
         self.model = model
         self.points = points
         self.radius_bounds = radius_bounds
         self.remove_J2 = remove_J2
-        self.distribution = config['distribution'][0]
+        self.distribution = config["distribution"][0]
 
         if self.radius_bounds is None:
-            min_radius = self.config['radius_min'][0]
-            max_radius = self.config['radius_max'][0]
+            min_radius = self.config["radius_min"][0]
+            max_radius = self.config["radius_max"][0]
 
             # If training data was augmented data take the largest radius
-            augment_data = self.config.get('augment_data_config', [{}])[0]
-            extra_max_radius = augment_data.get('radius_max', [0])[0]
+            augment_data = self.config.get("augment_data_config", [{}])[0]
+            extra_max_radius = augment_data.get("radius_max", [0])[0]
             max_radius = np.max([max_radius, extra_max_radius])
 
             self.radius_bounds = [min_radius, max_radius]
@@ -41,11 +47,20 @@ class DataInterface:
         planet = self.config["planet"][0]
         grav_file = self.config["grav_file"][0]
 
-        trajectory = self.distribution(planet, self.radius_bounds, self.points, **self.config)
-        get_analytic_data_fcn = self.config['gravity_data_fcn'][0]
-        
+        grav_file = make_windows_path_posix(grav_file)
+
+        trajectory = self.distribution(
+            planet,
+            self.radius_bounds,
+            self.points,
+            **self.config,
+        )
+        get_analytic_data_fcn = self.config["gravity_data_fcn"][0]
+
         x_unscaled, a_unscaled, u_unscaled = get_analytic_data_fcn(
-            trajectory, grav_file, **self.config
+            trajectory,
+            grav_file,
+            **self.config,
         )
 
         self.positions = x_unscaled
@@ -54,20 +69,26 @@ class DataInterface:
 
     def get_PINN_data(self):
         positions = self.positions
-        self.predicted_accelerations =  self.model.compute_acceleration(positions).numpy().astype(float)
-        self.predicted_potentials =  self.model.compute_potential(positions).numpy().astype(float)
+        self.predicted_accelerations = (
+            self.model.compute_acceleration(positions).numpy().astype(float)
+        )
+        self.predicted_potentials = (
+            self.model.compute_potential(positions).numpy().astype(float)
+        )
 
     def get_J2_data(self):
         planet = self.config["planet"][0]
         grav_file = self.config["grav_file"][0]
 
         trajectory = RandomDist(planet, self.radius_bounds, self.points, **self.config)
-        get_analytic_data_fcn = self.config['gravity_data_fcn'][0]
+        get_analytic_data_fcn = self.config["gravity_data_fcn"][0]
         config_mod = self.config.copy()
-        config_mod['max_deg'] = [2]
-        
+        config_mod["max_deg"] = [2]
+
         x_unscaled, a_unscaled, u_unscaled = get_analytic_data_fcn(
-            trajectory, grav_file, **config_mod
+            trajectory,
+            grav_file,
+            **config_mod,
         )
 
         if self.remove_J2:
@@ -76,8 +97,7 @@ class DataInterface:
             self.LF_potentials = u_unscaled
         else:
             self.LF_accelerations = np.zeros_like(x_unscaled)
-            self.LF_potentials = np.zeros_like(x_unscaled[:,0:1])
-        
+            self.LF_potentials = np.zeros_like(x_unscaled[:, 0:1])
 
     def gather_data(self):
         self.get_data()
