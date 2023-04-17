bc2626dccdb1891b9100721bdad7d1fcd22c12e6
John M <john.r.martin@colorado.edu>
2023-04-17 17:54:01 -0600

    Update poly visualization

--- GravNN/Visualization/PolyVisualization.py
+++ GravNN/Visualization/PolyVisualization.py
@@ -1,78 +1,106 @@
-from GravNN.Visualization.VisualizationBase import VisualizationBase
-from GravNN.Support.transformations import sphere2cart
-import numpy as np
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-import matplotlib.pyplot as plt
-import copy
 import os
-from enum import Enum
-from matplotlib.colors import LogNorm, SymLogNorm, Normalize
+
+import matplotlib.pyplot as plt
+import numpy as np
+import trimesh
 from matplotlib import cm
-
-
-from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.colors import Normalize, SymLogNorm
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-class PolyVisualization(VisualizationBase):
-    def __init__(self, unit='m/s^2', **kwargs):
-        """Visualization class for polyhedral shape models
+from GravNN.Visualization.VisualizationBase import VisualizationBase
 
-        Args:
-            unit (str, optional): acceleration unit ('m/s^2' or 'mGal'). Defaults to 'm/s^2'.
-        """
+
+class PolyVisualization(VisualizationBase):
+    def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.file_directory += os.path.splitext(os.path.basename(__file__))[0] + "/"
-        if unit == "mGal":
-            # https://en.wikipedia.org/wiki/Gal_(unit)
-            # 1 Gal == 0.01 m/s^2
-            # 1 mGal == 1E-2 * 10E-3 = 10E-5 or 10000 mGal per m/s^2
-            self.scale = 10000.0
-        elif unit == "m/s^2":
-            self.scale = 1.0
         pass
 
-    def plot_polyhedron(self, mesh, accelerations, label=None, vlim=None,log=False, cbar=True, surface_colors=True):
+    def plot_polyhedron(
+        self,
+        obj_file,
+        accelerations=None,
+        label=None,
+        vlim=None,
+        log=False,
+        cbar=True,
+        surface_colors=True,
+    ):
         fig, ax = self.new3DFig()
 
-        if surface_colors:
-            cmap = plt.get_cmap('bwr')
-            tri = Poly3DCollection(mesh.triangles*1000, cmap=cmap)
+        filename, file_extension = os.path.splitext(obj_file)
+        mesh = trimesh.load_mesh(obj_file, file_type=file_extension[1:])
 
-            scaled_acceleration = (accelerations - np.min(accelerations))/(np.max(accelerations) - np.min(accelerations))
-            for i in range(len(mesh.faces)):
-                color = np.array(cmap(int(scaled_acceleration[i]*255)))*255
-                mesh.visual.face_colors[i] = color
-            tri.set_facecolor(mesh.visual.face_colors/255)
-            tri.set_edgecolor(mesh.visual.face_colors/255)
+        if surface_colors and accelerations is not None:
+            cmap = plt.get_cmap("bwr")
+            tri = Poly3DCollection(
+                mesh.triangles * 1000,
+                cmap=cmap,
+                alpha=0.4,
+                # shade=True,
+            )
+
+            x = np.linalg.norm(accelerations, axis=1)
+
+            # Normalize the color from 0 - 1
+            x_normal = (x - x.min()) / (x.max() - x.min())
+            mesh.visual.face_colors = cmap(x_normal)
+
+            # place color on the collection
+            tri.set_facecolor(mesh.visual.face_colors / 255)
+            tri.set_edgecolor(mesh.visual.face_colors / 255)
         else:
-            cmap = plt.get_cmap('Greys')
-            tri = Poly3DCollection(mesh.triangles*1000, cmap=cmap)
+            cmap = plt.get_cmap("Greys")
+            tri = Poly3DCollection(
+                mesh.triangles * 1000,
+                cmap=cmap,
+                alpha=0.4,
+                # shade=True,
+            )
 
-        p = ax.add_collection3d(tri)
-        min_lim = np.min(mesh.vertices*1000)
-        max_lim = np.max(mesh.vertices*1000)
-        ax.axes.set_xlim3d(left=min_lim, right=max_lim) 
-        ax.axes.set_ylim3d(bottom=min_lim, top=max_lim) 
-        ax.axes.set_zlim3d(bottom=min_lim, top=max_lim) 
-        vlim_min = np.min(accelerations)
-        vlim_max = np.max(accelerations)
-        if log:
-            norm = SymLogNorm(linthresh=1E-4, vmin=vlim_min, vmax=vlim_max)
-        else:
-            norm = Normalize(vmin=vlim_min, vmax=vlim_max)
-        arg = cm.ScalarMappable(norm=norm, cmap=cmap)
-        ticks = np.linspace(vlim_min, vlim_max, 5)
+        ax.add_collection3d(tri)
+        min_lim = np.min(mesh.vertices * 1000)
+        max_lim = np.max(mesh.vertices * 1000)
+        ax.axes.set_xlim3d(left=min_lim, right=max_lim)
+        ax.axes.set_ylim3d(bottom=min_lim, top=max_lim)
+        ax.axes.set_zlim3d(bottom=min_lim, top=max_lim)
 
-        if cbar: 
-            cBar = plt.colorbar(arg,  pad=0.20, fraction=0.15, norm=norm)#ticks=ticks,boundaries=vlim,
+        if cbar and accelerations is not None:
+            vlim_min = np.min(x)
+            vlim_max = np.max(x)
+            if log:
+                norm = SymLogNorm(linthresh=1e-4, vmin=vlim_min, vmax=vlim_max)
+            else:
+                norm = Normalize(vmin=vlim_min, vmax=vlim_max)
+
+            arg = cm.ScalarMappable(norm=norm, cmap=cmap)
+            cBar = plt.colorbar(
+                arg,
+                pad=0.20,
+                fraction=0.15,
+                norm=norm,
+            )  # ticks=ticks,boundaries=vlim,
             if label is not None:
                 cBar.ax.set_ylabel(label)
 
         return fig, ax
 
-    def plot_position_data(self, data, alpha=1.0, color='blue'):
-        x = data#/1000.0
-        ax = plt.gca()
-        plt.gcf().axes[0].scatter(x[:,0], x[:,1], x[:,2],s=1, c=color, alpha=alpha)
-        # ax = plt.figure().add_subplot(projection='3d')
-        # ax.scatter(x[:,0], x[:,1], x[:,2], s=1)
+    def plot_position_data(self, data, alpha=1.0, color="blue"):
+        x = data  # /1000.0
+        plt.gca()
+        plt.gcf().axes[0].scatter(x[:, 0], x[:, 1], x[:, 2], s=1, c=color, alpha=alpha)
+
+
+if __name__ == "__main__":
+    from GravNN.CelestialBodies.Asteroids import Eros
+    from GravNN.GravityModels.Polyhedral import Polyhedral
+    from GravNN.Trajectories.SurfaceDist import SurfaceDist
+
+    planet = Eros()
+    traj = SurfaceDist(planet, planet.obj_8k)
+    gravity_model = Polyhedral(planet, planet.obj_8k, traj).load()
+    acc = gravity_model.accelerations
+
+    vis = PolyVisualization()
+    vis.plot_polyhedron(planet.obj_8k, acc)
+    plt.show()
