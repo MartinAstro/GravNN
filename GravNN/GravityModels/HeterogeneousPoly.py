0c564aac6342f186556db8475378e48da445c5d8
John M <john.r.martin@colorado.edu>
2023-04-18 10:24:57 -0600

    Add a new polyhedral gravity model with mascons

---
+++ GravNN/GravityModels/HeterogeneousPoly.py
@@ -0,0 +1,95 @@
+import copy
+
+from GravNN.GravityModels.PointMass import PointMass
+from GravNN.GravityModels.Polyhedral import Polyhedral
+
+
+class HeterogeneousPoly(Polyhedral):
+    def __init__(self, celestial_body, obj_file, trajectory=None):
+        super().__init__(celestial_body, obj_file, trajectory)
+
+        self.point_mass_list = []
+        self.offset_list = []
+
+    def add_point_mass(self, point_mass, r_offset):
+        self.point_mass_list.append(point_mass)
+        self.offset_list.append(r_offset)
+
+    def load(self, override=False):
+        # get homoegenous poly accelerations
+        super().load(override)
+        self.a_poly = copy.deepcopy(self.accelerations)
+        self.u_poly = copy.deepcopy(self.potentials)
+
+        x = self.positions
+        a_poly = self.accelerations
+        u_poly = self.potentials
+
+        for i in range(len(self.point_mass_list)):
+            r_offset = self.offset_list[i]
+            x_pm = x - r_offset
+
+            a_pm = self.point_mass_list[i].compute_acceleration(x_pm)
+            u_pm = self.point_mass_list[i].compute_potential(x_pm)
+
+            a_poly += a_pm
+            u_poly += u_pm
+
+        self.accelerations = a_poly
+        self.potentials = u_poly
+
+    def compute_acceleration(self, x):
+        a_poly = super().compute_acceleration(x)
+
+        for i in range(len(self.point_mass_list)):
+            r_offset = self.offset_list[i]
+            x_pm = x - r_offset
+            a_pm = self.point_mass_list[i].compute_acceleration(x_pm)
+            a_poly += a_pm
+
+    def compute_potentials(self, x):
+        u_poly = super().compute_potential(x)
+
+        for i in range(len(self.point_mass_list)):
+            r_offset = self.offset_list[i]
+            x_pm = x - r_offset
+            u_pm = self.point_mass_list[i].compute_potential(x_pm)
+            u_poly += u_pm
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    from GravNN.CelestialBodies.Asteroids import Eros
+    from GravNN.Trajectories.SurfaceDist import SurfaceDist
+
+    planet = Eros()
+
+    traj = SurfaceDist(planet, planet.obj_8k)
+    gravity_model = HeterogeneousPoly(planet, planet.obj_8k, traj)
+
+    mass_1 = Eros()
+    mass_1.mu = planet.mu / 10
+    r_offset_1 = [planet.radius / 3, 0, 0]
+
+    mass_2 = Eros()
+    mass_2.mu = -planet.mu / 10
+    r_offset_2 = [-planet.radius / 3, 0, 0]
+
+    point_mass_1 = PointMass(mass_1, traj)
+    point_mass_2 = PointMass(mass_2, traj)
+
+    gravity_model.add_point_mass(point_mass_1, r_offset_1)
+    gravity_model.add_point_mass(point_mass_2, r_offset_2)
+
+    gravity_model.load()
+
+    from GravNN.Visualization.PolyVisualization import PolyVisualization
+
+    vis = PolyVisualization()
+    vis.plot_polyhedron(planet.obj_8k, gravity_model.accelerations)
+    plt.gca().scatter(r_offset_1[0], r_offset_1[1], r_offset_1[2], s=400)
+    plt.gca().scatter(r_offset_2[0], r_offset_2[1], r_offset_2[2], s=400)
+    vis.plot_polyhedron(planet.obj_8k, gravity_model.a_poly)
+
+    plt.show()
