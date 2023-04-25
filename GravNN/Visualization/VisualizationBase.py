081172a03a15b466e12586f139ae37e98901487a
John M <john.r.martin@colorado.edu>
2023-04-25 16:47:27 -0600

    Redo the figure sizing in VisBase

--- GravNN/Visualization/VisualizationBase.py
+++ GravNN/Visualization/VisualizationBase.py
@@ -7,20 +7,22 @@ import numpy as np
 
 
 def convert_string(string):
-    string = string.replace(".","_")
+    string = string.replace(".", "_")
     string = string.replace("[", "_")
     string = string.replace("]", "_")
     string = string.replace(" ", "")
     string = string.replace(",", "_")
     return string
 
-class VisualizationBase(ABC):
 
-    def __init__(self,
-                 save_directory=None,
-                 halt_formatting=False,
-                 formatting_style=None,
-                 **kwargs):
+class VisualizationBase(ABC):
+    def __init__(
+        self,
+        save_directory=None,
+        halt_formatting=False,
+        formatting_style=None,
+        **kwargs,
+    ):
         """Default visualization base class. Generates consistent style formatting for
         all figures, offers information about proper figure sizes.
 
@@ -31,72 +33,117 @@ class VisualizationBase(ABC):
                                                 should be applied. Defaults to False.
         """
         if save_directory is None:
-            self.file_directory = os.path.abspath('.') +"/Plots/"
+            self.file_directory = os.path.abspath(".") + "/Plots/"
         else:
             self.file_directory = save_directory
 
+        self.golden_ratio = (5**0.5 - 1) / 2  # = 0.61
+        self.silver_ratio = 1 / (1 + np.sqrt(2))  # = 0.41
+
+        # Can be acquired loading the layouts package using
+        # \printinunitsof{in}\prntlen{\textwidth}
+        # \printinunitsof{in}\prntlen{\textheight}
+        # \printinunitsof{in}\prntlen{\abovecaptionskip}
+
+        SPRINGER_NATURE_WIDTH = 4.67596  # inches
+        SPRINGER_NATURE_HEIGHT = 7.64914  # inches
+        SPRINGER_CAPTION_PADDING = 0.03113  # inches
+
+        width = SPRINGER_NATURE_WIDTH
+        height = SPRINGER_NATURE_HEIGHT
+        padding = SPRINGER_CAPTION_PADDING
+
+        self.h_full = height
+        self.h_half = height / 2
+        self.h_tri = height / 3
+        self.h_quad = height / 4
+
+        self.h_full_pad = height - padding
+        self.h_half_pad = height / 2 - padding
+        self.h_tri_pad = height / 3 - padding
+        self.h_quad_pad = height / 4 - padding
+
+        self.w_full = width
+        self.w_half = width / 2
+        self.w_tri = width / 3
+        self.w_quad = width / 4
 
         # (width, height)
-        golden_ratio = (5**.5 - 1) / 2    # = 0.61
-        silver_ratio = 1/(1 + np.sqrt(2)) # = 0.41
+        self.full_page_default = (self.w_full, self.w_full * self.golden_ratio)
+        self.half_page_default = (self.w_half, self.w_half * self.golden_ratio)
+        self.tri_page_default = (self.w_tri, self.w_tri * self.golden_ratio)
+        self.quarter_page_default = (self.w_quad, self.w_quad * self.golden_ratio)
 
-        self.full_page_default = (6.3, 6.3*0.8)
-        self.half_page_default = (3.15, 3.15*0.8)
-        self.tri_page_default = (2.1, 2.1*0.8)
-        self.quarter_page_default = (1.57, 1.57*0.8)
+        self.full_page_silver = (self.w_full, self.w_full * self.silver_ratio)
+        self.half_page_silver = (self.w_half, self.w_half * self.silver_ratio)
+        self.tri_page_silver = (self.w_tri, self.w_tri * self.silver_ratio)
+        self.quarter_page_silver = (self.w_quad, self.w_quad * self.silver_ratio)
 
-        self.full_page_square = (6.3, 6.3)
-        self.half_page_square = (3.15, 3.15)
-        self.tri_page_square = (2.1, 2.1)
-        self.quarter_page_square = (1.57, 1.57)
-
-        self.full_page_silver = (6.3, 6.3*silver_ratio)
-        self.half_page_silver = (3.15, 3.15*silver_ratio) #
-        self.tri_page_silver = (2.1, 2.1*silver_ratio)
-        self.quarter_page_silver = (1.57, 1.57*silver_ratio)
-
-        self.full_page_golden = (6.3, 6.3*golden_ratio)
-        self.half_page_golden = (3.15, 3.15*golden_ratio) #
-        self.tri_page_golden = (2.1, 2.1*golden_ratio)
-        self.quarter_page_golden = (1.57, 1.57*golden_ratio)
+        self.full_page_golden = (self.w_full, self.w_full * self.golden_ratio)
+        self.half_page_golden = (self.w_half, self.w_half * self.golden_ratio)
+        self.tri_page_golden = (self.w_tri, self.w_tri * self.golden_ratio)
+        self.quarter_page_golden = (self.w_quad, self.w_quad * self.golden_ratio)
 
         # AAS textwidth is 6.5
-        self.AIAA_half_page = (3.25, 3.25*golden_ratio) #
-        self.AIAA_half_page_heuristic = (4, 4*golden_ratio) #
-        if formatting_style == 'AIAA':
-            self.AIAA_full_page = (6.5, 6.5*golden_ratio)
+        self.AIAA_half_page = (3.25, 3.25 * self.golden_ratio)  #
+        self.AIAA_half_page_heuristic = (4, 4 * self.golden_ratio)  #
+        if formatting_style == "AIAA":
+            self.AIAA_full_page = (6.5, 6.5 * self.golden_ratio)
 
         # Set default figure size
-        self.fig_size = (5,3.5) #(3, 1.8) is half page.
+        self.fig_size = self.full_page_golden
 
         # default figure styling
-        plt.rc('font', size= 10.0)
-        plt.rc('font', family='serif')
-        plt.rc('axes', prop_cycle=mpl.cycler(color=[
-            'blue', 'green', 'red',
-            'orange', 'gold',  'salmon',
-            'lime', 'magenta','lavender',
-            'yellow', 'black', 'lightblue',
-            'darkgreen', 'pink', 'brown',
-            'teal', 'coral',  'turquoise',
-            'tan', 'gold']))
-        plt.rc('axes.grid', axis='both')
-        plt.rc('axes.grid', which='both')
-        plt.rc('axes', grid=True)
-        plt.rc('grid', linestyle='--')
-        plt.rc('grid', linewidth='0.1')
-        plt.rc('grid', color='.25')
-        plt.rc('text', usetex=True)
+        plt.rc("font", size=6.0)
+        plt.rc("font", family="serif")
+        plt.rc("figure", autolayout=True)
+        plt.rc("savefig", pad_inches=0.0)
+        plt.rc("lines", linewidth=0.5)
+
+        plt.rc(
+            "axes",
+            prop_cycle=mpl.cycler(
+                color=[
+                    "blue",
+                    "green",
+                    "red",
+                    "orange",
+                    "gold",
+                    "salmon",
+                    "lime",
+                    "magenta",
+                    "lavender",
+                    "yellow",
+                    "black",
+                    "lightblue",
+                    "darkgreen",
+                    "pink",
+                    "brown",
+                    "teal",
+                    "coral",
+                    "turquoise",
+                    "tan",
+                    "gold",
+                ],
+            ),
+        )
+        plt.rc("axes.grid", axis="both")
+        plt.rc("axes.grid", which="both")
+        plt.rc("axes", grid=True)
+        plt.rc("grid", linestyle="--")
+        plt.rc("grid", linewidth="0.1")
+        plt.rc("grid", color=".25")
+        plt.rc("text", usetex=True)
 
         if halt_formatting:
-            plt.rc('text', usetex=False)
+            plt.rc("text", usetex=False)
 
         return
 
-    def new3DFig(self, unit='m', **kwargs):
-        figsize = kwargs.get('fig_size', self.fig_size)
+    def new3DFig(self, unit="m", **kwargs):
+        figsize = kwargs.get("fig_size", self.fig_size)
         fig = plt.figure(num=None, figsize=figsize)
-        ax = fig.add_subplot(111, projection='3d')
+        ax = fig.add_subplot(111, projection="3d")
         # ax.set_xlabel(r'$x$ ('+unit+r')')
         # ax.set_ylabel(r'$y$ ('+unit+r')')
         # ax.set_zlabel(r'$z$ ('+unit+r')')
@@ -110,7 +157,7 @@ class VisualizationBase(ABC):
         return fig, ax
 
     def save(self, fig, name):
-        #If the name is an absolute path -- save to the path
+        # If the name is an absolute path -- save to the path
         if os.path.isabs(name):
             try:
                 plt.figure(fig.number)
@@ -124,9 +171,9 @@ class VisualizationBase(ABC):
         directory = os.path.abspath(os.path.dirname(self.file_directory + name))
         directory = convert_string(directory)
 
-        filename =  os.path.basename(name)
+        filename = os.path.basename(name)
         os.makedirs(directory, exist_ok=True)
         try:
-            plt.savefig(directory+"/" + filename, pad_inches=0.1)
+            plt.savefig(directory + "/" + filename)
         except Exception as e:
             print(f"Couldn't save {filename}\n {e}")
