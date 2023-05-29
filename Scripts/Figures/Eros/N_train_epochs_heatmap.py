cd4e21d8aa66724ed7d6519a0afbc0e97a5b626f
John M <john.r.martin@colorado.edu>
2023-05-29 16:15:21 -0600

    Improve plots for heatmaps

--- Scripts/Figures/Eros/N_train_epochs_heatmap.py
+++ Scripts/Figures/Eros/N_train_epochs_heatmap.py
@@ -6,16 +6,6 @@ from GravNN.Visualization.HeatmapVisualizer import Heatmap3DVisualizer
 
 def all_plots(df, directory, v_min, v_max):
     vis = Heatmap3DVisualizer(df)
-    query = "num_units == 10"
-    vis.plot(
-        x="Epochs",
-        y="Samples",
-        z="percent_mean",
-        vmin=v_min,
-        vmax=v_max,
-        query=query,
-    )
-    vis.save(plt.gcf(), f"{directory}/Eros_NvE_10.pdf")
 
     query = "num_units == 20"
     vis.plot(
@@ -55,12 +45,13 @@ def PINN_III():
     df_file = "Data/Dataframes/eros_PINN_III_hparams_metrics.data"
     df = pd.read_pickle(df_file)
     df = df.rename(columns={"epochs": "Epochs", "N_train": "Samples"})
-
+    df = df.query("num_units != 10")
     df.percent_mean = df.percent_mean.astype(float)
     df.percent_mean = df.percent_mean * 100
 
     v_min = df["percent_mean"].min()
-    v_max = df.nlargest(2, "percent_mean")["percent_mean"].values[1]
+    # v_max = df.nlargest(2, "percent_mean")["percent_mean"].values[1]
+    v_max = 0.75
 
     directory = "PINNIII"
     all_plots(df, directory, v_min, v_max)
@@ -70,17 +61,21 @@ def PINN_II():
     df_file = "Data/Dataframes/eros_PINN_II_hparams_metrics.data"
     df = pd.read_pickle(df_file)
     df = df.rename(columns={"epochs": "Epochs", "N_train": "Samples"})
+    df = df.query("num_units != 10")
+
     df.percent_mean = df.percent_mean * 100
 
     df_file_PINN_III = "Data/Dataframes/eros_PINN_III_hparams_metrics.data"
     df_PINN_III = pd.read_pickle(df_file_PINN_III)
+    df_PINN_III = df_PINN_III.query("num_units != 10")
 
     # scale by PINN III Results
     df_PINN_III.percent_mean = df_PINN_III.percent_mean * 100
     df_PINN_III.percent_mean = df_PINN_III.percent_mean.astype(float)
 
     v_min = df_PINN_III["percent_mean"].min()
-    v_max = df_PINN_III.nlargest(2, "percent_mean")["percent_mean"].values[1]
+    # v_max = df_PINN_III.nlargest(2, "percent_mean")["percent_mean"].values[1]
+    v_max = 0.75
 
     directory = "PINNII"
     all_plots(df, directory, v_min, v_max)
