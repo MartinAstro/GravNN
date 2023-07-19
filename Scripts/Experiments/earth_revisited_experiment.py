6ac1b740b2abbadb5bebaeba12f133446ca35967
John M <john.r.martin@colorado.edu>
2023-07-19 14:07:40 -0600

    Update earth_revised

--- Scripts/Experiments/earth_revisited_experiment.py
+++ Scripts/Experiments/earth_revisited_experiment.py
@@ -1,15 +1,14 @@
 import os
 from pprint import pprint
 
-import tensorflow as tf
-
 from GravNN.Networks.Configs import *
 from GravNN.Networks.script_utils import save_training
 from GravNN.Networks.utils import configure_run_args
 
 os.environ["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] = "YES"
 
-df_file = "Data/Dataframes/earth_revisited_032723.data"
+
+df_file = "Data/Dataframes/earth_revisited_071923.data"
 
 
 def main():
@@ -18,40 +17,36 @@ def main():
     config.update(ReduceLrOnPlateauConfig())
 
     hparams = {
-        # "N_dist": [5000000],
-        # "N_train": [4900000],
-        # "N_val": [100000],
-        "N_dist": [50000],
-        "N_train": [45000],
-        "N_val": [5000],
+        "N_dist": [5000000],
+        "N_train": [4900000],
+        "N_val": [100000],
         "num_units": [20],
         "loss_fcns": [["percent", "rms"]],
         "jit_compile": [True],
         "lr_anneal": [False],
         "eager": [False],
-        "learning_rate": [0.0001],
-        "batch_size": [2**12],
-        "epochs": [20],
         "preprocessing": [["pines", "r_inv"]],
         "patience": [5000],
         "dropout": [0.0],
-        # "batch_size": [4500],
         "min_delta": [0.01],
         "acc_noise": [0.0],
-        # "preprocessing": [["pines", "r_inv", "fourier"]],
         "PINN_constraint_fcn": ["pinn_a"],
-        # "fourier_features": [5],
-        "fourier_sigma": [1],
-        # "shared_freq": [False],
-        # "shared_offset": [False],
-        # "trainable": [True],
-        "base_2_init": [True],
-        "fourier_features": [10],
-        "trainable": [False],
-        "shared_freq": [True],
-        "shared_offset": [True],
+        "learning_rate": [0.0001],
+        "batch_size": [2**13],
+        "epochs": [100],
+        # "learning_rate": [0.01],
+        # "batch_size": [2**22],
+        # "epochs": [10000],
+        # "preprocessing": [["pines", "r_inv", "fourier"]],
+        # "fourier_sigma": [1],
+        # "base_2_init": [True],
+        # "fourier_features": [10],
+        # "trainable": [False],
+        # "shared_freq": [True],
+        # "shared_offset": [True],
         "tanh_k": [3],
         "tanh_r": [Earth().radius * 10],
+        "init_file": [2460145.335219605],
         # "augment_data_config": [
         #     {
         #         "N_dist": [105000],
@@ -62,20 +57,6 @@ def main():
         #     },
         # ],
     }
-    args = configure_run_args(config, hparams)
-    configs = [run(*args[0])]
-    save_training(df_file, configs)
-
-    # run with larger BS for longer
-    tf.keras.backend.clear_session()
-    hparams.update(
-        {
-            "init_file": [configs[0]["id"][0]],
-            "batch_size": [2**16],
-            "learning_rate": [0.01],
-            "epochs": [20],
-        },
-    )
     args = configure_run_args(config, hparams)
     configs = [run(*args[0])]
     save_training(df_file, configs)
