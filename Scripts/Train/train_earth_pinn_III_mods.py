079031e7ae93ce881660918c4bc1a685f8146c5c
John M <john.r.martin@colorado.edu>
2023-10-26 15:54:15 -0400

    PINN III modifications script

--- Scripts/Train/train_earth_pinn_III_mods.py
+++ Scripts/Train/train_earth_pinn_III_mods.py
@@ -1,4 +1,3 @@
-import multiprocessing as mp
 import os
 from pprint import pprint
 
@@ -7,70 +6,6 @@ from GravNN.Networks.script_utils import save_training
 from GravNN.Networks.utils import configure_run_args
 
 os.environ["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] = "YES"
-
-
-def main():
-    threads = 2
-    config = get_default_earth_config()
-    config.update(PINN_III())
-    config.update(ReduceLrOnPlateauConfig())
-    df_file = "Data/Dataframes/earth_loss_fcn_experiment.data"
-    hparams = {
-        "N_dist": [10000],
-        "N_train": [5000],
-        "N_val": [1000],
-        "num_units": [20],
-        "radius_max": [Earth().radius * 15],
-        "loss_fcns": [["rms"], ["percent"]],
-        "jit_compile": [True],
-        "lr_anneal": [False],
-        "eager": [False],
-        "learning_rate": [0.001],
-        "dropout": [0.0],
-        "batch_size": [2**18],
-        "epochs": [5000],
-        "acc_noise": [0.0],
-        "preprocessing": [["pines", "r_inv"]],
-        "PINN_constraint_fcn": ["pinn_a"],
-        # "final_layer_initializer": ["glorot_uniform"],
-        "scale_nn_potential": [False],
-        "trainable": [False],
-        "fuse_models": [False],
-        "deg_removed": [2],
-        "enforce_bc": [False],
-    }
-    args = configure_run_args(config, hparams)
-    with mp.Pool(threads) as pool:
-        results = pool.starmap_async(run, args)
-        configs = results.get()
-    save_training(df_file, configs)
-
-    df_file = "Data/Dataframes/earth_scaled_potential_experiment.data"
-    hparams.update(
-        {
-            "scale_nn_potential": [False, True],
-            "loss_fcns": [["percent"]],
-        },
-    )
-    args = configure_run_args(config, hparams)
-    with mp.Pool(threads) as pool:
-        results = pool.starmap_async(run, args)
-        configs = results.get()
-    save_training(df_file, configs)
-
-    df_file = "Data/Dataframes/earth_boundary_conditions_experiment.data"
-    hparams.update(
-        {
-            "scale_nn_potential": [True],
-            "loss_fcns": [["percent"]],
-            "enforce_bc": [True, False],
-        },
-    )
-    args = configure_run_args(config, hparams)
-    with mp.Pool(threads) as pool:
-        results = pool.starmap_async(run, args)
-        configs = results.get()
-    save_training(df_file, configs)
 
 
 def run(config):
@@ -95,6 +30,63 @@ def run(config):
 
     print(f"Model ID: [{model.config['id']}]")
     return model.config
+
+
+def run_and_save(df_file, hparams, config):
+    args = configure_run_args(config, hparams)
+    # return
+    configs = [run(args[0][0])]
+    save_training(df_file, configs)
+
+
+def main():
+    config = get_default_eros_config()
+    config.update(PINN_III())
+    config.update(ReduceLrOnPlateauConfig())
+    hparams = {
+        "N_dist": [6000],
+        "N_train": [1000],
+        "N_val": [1000],
+        "num_units": [20],
+        "radius_max": [Eros().radius * 15],
+        "loss_fcns": [["mse"]],
+        "jit_compile": [True],
+        "eager": [False],
+        # "jit_compile": [False],
+        # "eager": [True],
+        "lr_anneal": [False],
+        "learning_rate": [0.001],
+        "dropout": [0.0],
+        "batch_size": [2**18],
+        "epochs": [5000],
+        "acc_noise": [0.0],
+        "preprocessing": [["pines", "r_inv"]],
+        "PINN_constraint_fcn": ["pinn_a"],
+        "scale_nn_potential": [False],
+        "trainable": [False],
+        "fuse_models": [False],
+        "enforce_bc": [False],
+        "uniform_volume": [False],
+    }
+
+    df_file = "Data/Dataframes/pinn_III_mods_RMS.data"
+    # run_and_save(df_file, hparams, config)
+
+    df_file = "Data/Dataframes/pinn_III_mods_percent.data"
+    hparams.update({"loss_fcns": [["percent"]]})
+    # run_and_save(df_file, hparams, config)
+
+    df_file = "Data/Dataframes/pinn_III_mods_scaling.data"
+    hparams.update({"scale_nn_potential": [True]})
+    run_and_save(df_file, hparams, config)
+
+    df_file = "Data/Dataframes/pinn_III_mods_BC.data"
+    hparams.update({"enforce_bc": [True], "tanh_k": [0.1]})
+    run_and_save(df_file, hparams, config)
+
+    df_file = "Data/Dataframes/pinn_III_mods_fuse.data"
+    hparams.update({"fuse_models": [True]})
+    run_and_save(df_file, hparams, config)
 
 
 if __name__ == "__main__":
