93e6bcba4d415fbacabd6bd3464d7e2ec1c03a0b
John M <john.r.martin@colorado.edu>
2023-11-06 12:05:48 -0500

    Update regression

--- GravNN/Regression/BLLS.py
+++ GravNN/Regression/BLLS.py
@@ -1,7 +1,7 @@
 import numpy as np
 from SHRegression import SHRegression
 
-from GravNN.Regression.utils import format_coefficients
+from GravNN.Regression.utils import format_coefficients, populate_removed_degrees, save
 
 
 def iterate_lstsq(M, aVec, iterations):
@@ -18,8 +18,7 @@ class BLLS:
     def __init__(self, max_deg, planet, remove_deg=-1):
         self.N = max_deg  # Degree
         self.remove_deg = remove_deg
-
-        self.SHRegressor = SHRegression(max_deg, planet.radius, planet.mu)
+        self.SHRegressor = SHRegression(max_deg, planet.radius, planet.mu, remove_deg)
 
     def update(self, rVec, aVec, iterations=5):
         self.rVec1D = rVec.reshape((-1,))
@@ -57,35 +56,42 @@ class BLLS_PM:
         return results
 
 
-def main():
+def test_setup(max_true_degree, regress_degree, remove_degree):
+    import tempfile
     import time
 
     from GravNN.CelestialBodies.Planets import Earth
     from GravNN.GravityModels.SphericalHarmonics import SphericalHarmonics, get_sh_data
     from GravNN.Trajectories import DHGridDist
 
-    max_true_deg = 10
-    regress_deg = 5
-    remove_deg = -1
-    # remove_deg = 0 # C20 is very close, C22 isn't that close
+    MAX_TRUE_DEG = max_true_degree
+    REGRESS_DEG = regress_degree
+    REMOVE_DEG = remove_degree
 
     planet = Earth()
-    sh_EGM2008 = SphericalHarmonics(planet.sh_file, regress_deg)
+    sh_EGM2008 = SphericalHarmonics(planet.sh_file, REGRESS_DEG)
 
-    trajectory = DHGridDist(planet, sh_EGM2008.radEquator, 5)
-    # trajectory = RandomDist(planet, [planet.radius, planet.radius+420], 1000)
+    trajectory = DHGridDist(planet, sh_EGM2008.radEquator, 10)
 
     x, a, u = get_sh_data(
         trajectory,
         planet.sh_file,
-        max_deg=max_true_deg,
-        deg_removed=remove_deg,
+        max_deg=MAX_TRUE_DEG,
+        deg_removed=REMOVE_DEG,
     )
 
-    regressor = BLLS(regress_deg, planet, remove_deg)
+    regressor = BLLS(REGRESS_DEG, planet, REMOVE_DEG)
     start = time.time()
     results = regressor.update(x, a)
-    C_lm, S_lm = format_coefficients(results, regress_deg, remove_deg)
+    C_lm, S_lm = format_coefficients(results, REGRESS_DEG, REMOVE_DEG)
+    C_lm, S_lm = populate_removed_degrees(
+        C_lm,
+        S_lm,
+        sh_EGM2008.C_lm,
+        sh_EGM2008.S_lm,
+        REMOVE_DEG,
+    )
+
     print(time.time() - start)
 
     k = len(C_lm)
@@ -95,11 +101,79 @@ def main():
     C_lm_error = (C_lm_true - C_lm) / C_lm_true * 100
     S_lm_error = (S_lm_true - S_lm) / S_lm_true * 100
 
-    print(np.array2string(C_lm_error, precision=0))
-    print(np.array2string(S_lm_error, precision=0))
+    C_lm_error[np.isinf(C_lm_error)] = np.nan
+    S_lm_error[np.isinf(S_lm_error)] = np.nan
 
-    # regressor.save('C:\\Users\\John\\Documents\\Research\\ML_Gravity\\GravNN\\Files\\GravityModels\\Regressed\\some.csv')
-    # print(coefficients)
+    C_lm_avg_error = np.nanmean(np.abs(C_lm_error))
+    S_lm_avg_error = np.nanmean(np.abs(S_lm_error))
+
+    # Save coefficents to temporary file
+    with tempfile.NamedTemporaryFile() as tmpfile:
+        save(tmpfile.name, planet, C_lm, S_lm)
+        regressed_model = SphericalHarmonics(tmpfile.name, REGRESS_DEG)
+        accelerations = regressed_model.compute_acceleration(trajectory.positions)
+
+        x, a, u = get_sh_data(
+            trajectory,
+            planet.sh_file,
+            max_deg=MAX_TRUE_DEG,
+            deg_removed=-1,
+        )
+        error = (
+            np.linalg.norm(accelerations - a, axis=1) / np.linalg.norm(a, axis=1) * 100
+        )
+
+    # Print Metrics
+    print("\nC_LM_REGRESS\n")
+    print(np.array2string(C_lm, precision=1))
+    print("\nC_LM_TRUE\n")
+    print(np.array2string(C_lm_true, precision=1))
+    print("\nC_LM_ERROR\n")
+    print(np.array2string(C_lm_error, precision=1))
+
+    print("\nS_LM_REGRESS\n")
+    print(np.array2string(S_lm, precision=1))
+    print("\nS_LM_TRUE\n")
+    print(np.array2string(S_lm_true, precision=1))
+    print("\nS_LM_ERROR\n")
+    print(np.array2string(S_lm_error, precision=1))
+
+    print(f"\n AVERAGE CLM ERROR: {C_lm_avg_error} \n")
+    print(f"\n AVERAGE SLM ERROR: {S_lm_avg_error} \n")
+
+    print(f"\n ACCELERATION ERROR: {np.mean(error)}")
+
+    return C_lm_avg_error, S_lm_avg_error
+
+
+def main():
+    # C_err, S_err = test_setup(
+    #     max_true_degree=4,
+    #     regress_degree=4,
+    #     remove_degree=1,
+    #     )
+    # assert np.isclose(C_err, 1.207342327341248e-07)
+    # assert np.isclose(S_err, 2.5174006765704146e-09)
+
+    # C_err, S_err = test_setup(
+    #     max_true_degree=4,
+    #     regress_degree=4,
+    #     remove_degree=-1,
+    #     )
+    # assert np.isclose(C_err, 1.6180533607033576e-06)
+    # assert np.isclose(S_err, 4.275299330063149e-08)
+
+    test_setup(
+        max_true_degree=10,
+        regress_degree=4,
+        remove_degree=1,
+    )
+
+    test_setup(
+        max_true_degree=10,
+        regress_degree=4,
+        remove_degree=-1,
+    )
 
 
 if __name__ == "__main__":
--- GravNN/Regression/RLLS.py
+++ GravNN/Regression/RLLS.py
@@ -1,169 +1,84 @@
-import numpy as np
+import tempfile
 
+import numpy as np
+from SHRegression import SHRegression
+
+from GravNN.Regression.BLLS import BLLS
 from GravNN.Regression.utils import (
     format_coefficients,
-    getK,
-    populate_H_singular,
     populate_removed_degrees,
     save,
 )
+from GravNN.Support.ProgressBar import ProgressBar
 
 
 class RLLS:
     def __init__(self, max_deg, planet, x0, P0, Rk, remove_deg=-1):
         self.N = max_deg  # Degree
-        self.a = planet.radius
-        self.mu = planet.mu
-
+        self.planet = planet
+        self.remove_deg = remove_deg
         self.x_hat = x0.reshape((-1,))
         self.P_hat = P0
         self.Rk = Rk
-        self.remove_deg = remove_deg
+        self.SHRegressor = SHRegression(max_deg, planet.radius, planet.mu, remove_deg)
+        self.initialized = False
+        self.x0 = np.zeros((len(x0),))
 
-        self.rE = np.zeros((self.N + 2,))
-        self.iM = np.zeros((self.N + 2,))
-        self.rho = np.zeros((self.N + 3,))
+    def batch_start(self, x, a):
+        batch_regressor = BLLS(self.N, self.planet, self.remove_deg)
+        results = batch_regressor.update(x, a)
+        self.x_hat = results
 
-        self.A = np.zeros((self.N + 2, self.N + 2))
-
-        self.n1 = np.zeros((self.N + 2, self.N + 2))
-        self.n2 = np.zeros((self.N + 2, self.N + 2))
-
-        for i in range(0, self.N + 2):
-            if i == 0:
-                self.A[i, i] = 1.0
-            else:
-                self.A[i, i] = (
-                    np.sqrt((2.0 * i + 1.0) * getK(i) / (2.0 * i * getK(i - 1)))
-                    * self.A[i - 1, i - 1]
-                )
-
-            for m in range(0, i + 1):  # Check the plus one
-                if i >= m + 2:
-                    self.n1[i, m] = np.sqrt(
-                        ((2.0 * i + 1.0) * (2.0 * i - 1.0)) / ((i - m) * (i + m)),
-                    )
-                    self.n2[i, m] = np.sqrt(
-                        ((i + m - 1.0) * (2.0 * i + 1.0) * (i - m - 1.0))
-                        / ((i + m) * (i - m) * (2.0 * i - 3.0)),
-                    )
-
-    def update(self, rk, yk):
-        # Reminder: x is the vector of coefficients
-        # y is the acceleration measured
-        # Hk is the partial of da/dCoef(r) | r=r_k
+    def update_single(self, rk, yk):
+        # Load current estimates
         xk_m_1 = self.x_hat
         Pk_m_1 = self.P_hat
         Rk = self.Rk
 
-        Hk = populate_H_singular(
+        # Populate partials
+        Hk = self.SHRegressor.populate_H_singular(
             rk,
-            self.A,
-            self.n1,
-            self.n2,
-            self.N,
-            self.a,
-            self.mu,
             self.remove_deg,
         )
         sub_K_inv = np.linalg.inv(Rk + np.dot(Hk, np.dot(Pk_m_1, Hk.T)))
         Kk = np.dot(Pk_m_1, np.dot(Hk.T, sub_K_inv))
 
         Pk_sub = np.identity(len(xk_m_1)) - np.dot(Kk, Hk)
-        Pk = np.dot(Pk_sub, np.dot(Pk_m_1, Pk_sub.T))
 
+        assert np.all(np.isfinite(Pk_m_1))
+        assert np.all(np.isfinite(Hk))
+        assert np.all(np.isfinite(sub_K_inv))
+        assert np.all(np.isfinite(Kk))
+        assert np.all(np.isfinite(Pk_sub))
+
+        # update estimates
         self.x_hat = xk_m_1 + np.dot(Kk, yk - np.dot(Hk, xk_m_1))
-        self.P_hat = Pk
-
+        self.P_hat = np.dot(Pk_sub, np.dot(Pk_m_1, Pk_sub.T))
         return
 
+    def update(self, r, y, init_batch=0, history=False):
+        # Record time history of regressor
+        self.x_hat_hist = []
+        self.P_hat_hist = []
 
-def main():
-    import matplotlib.pyplot as plt
+        # Populate an initial guess
+        if init_batch > 0:
+            r_start = r[:init_batch, :]
+            y_start = y[:init_batch, :]
+            self.batch_start(r_start, y_start)
 
-    from GravNN.CelestialBodies.Planets import Earth
-    from GravNN.GravityModels.SphericalHarmonics import SphericalHarmonics, get_sh_data
-    from GravNN.Regression.BLLS import BLLS
-    from GravNN.Support.ProgressBar import ProgressBar
-    from GravNN.Trajectories import DHGridDist, RandomDist
+        # Update based on incoming data
+        pbar = ProgressBar(len(r), enable=True)
+        for i in range(init_batch, len(r)):
+            self.update_single(r[i], y[i])
+            pbar.update(i)
 
-    planet = Earth()
+            # optionally save
+            if history:
+                self.x_hat_hist.append(self.x_hat)
+                self.P_hat_hist.append(np.diag(self.P_hat).tolist())
 
-    max_true_deg = 10
-    regress_deg = 2
-    remove_deg = -1
-    batch_initialization = True
-    sh_EGM2008 = SphericalHarmonics(planet.sh_file, regress_deg)
-
-    trajectory = DHGridDist(planet, sh_EGM2008.radEquator, 360)
-    trajectory = RandomDist(planet, [planet.radius, planet.radius + 420000], 100000)
-
-    if remove_deg != -1:
-        x, a, u = get_sh_data(
-            trajectory,
-            planet.sh_file,
-            max_deg=max_true_deg,
-            deg_removed=remove_deg,
-        )
-        C_lm_start = 0.0
-    else:
-        x, a, u = get_sh_data(
-            trajectory,
-            planet.sh_file,
-            max_deg=max_true_deg,
-            deg_removed=-1,
-        )
-        C_lm_start = 1.0
-
-    N = regress_deg
-    M = remove_deg
-
-    # Initial Coefficient Estimates and Covariances
-    # N+1 accounts for the C00 terms
-    if batch_initialization:
-        batch_regressor = BLLS(N, planet, M)
-        results = batch_regressor.update(x[:100, :], a[:100, :])
-        x0 = results
-    else:
-        x0 = np.zeros(((N + 2) * (N + 1) - (M + 2) * (M + 1),))
-        x0[0] = C_lm_start
-
-    P0 = np.identity((N + 2) * (N + 1) - (M + 2) * (M + 1)) * 1e-6
-    P0[np.isnan(P0)] = 0.0
-    Rk = np.identity(3) * np.std(a) * 10
-
-    # Initialize the regressor
-    regressor = RLLS(N, planet, x0, P0, Rk, M)
-
-    # Record time history of regressor
-    x_hat_hist = []
-    P_hat_hist = []
-
-    pbar = ProgressBar(len(x), enable=True)
-    i = 0
-    for x_inst, y_inst in zip(x, a):
-        regressor.update(x_inst, y_inst)
-        i += 1
-        pbar.update(i)
-
-        x_hat_hist.append(regressor.x_hat)
-        P_hat_hist.append(np.diag(regressor.P_hat).tolist())
-
-    file_name = "/Users/johnmartin/Documents/GraduateSchool/Research/ML_Gravity/GravNN/Files/GravityModels/Regressed/Earth/test.csv"
-    save(file_name, planet, regressor.x_hat, regress_deg, remove_deg)
-
-    C_lm, S_lm = format_coefficients(regressor.x_hat, regressor.N, regressor.remove_deg)
-    C_lm, S_lm = populate_removed_degrees(
-        C_lm,
-        S_lm,
-        sh_EGM2008.C_lm,
-        sh_EGM2008.S_lm,
-        remove_deg,
-    )
-
-    plot_coef_history(x_hat_hist, P_hat_hist, sh_EGM2008, remove_deg, start_idx=0)
-
-    plt.show()
+        return
 
 
 def plot_coef_history(x_hat_hist, P_hat_hist, sh_EGM2008, remove_deg, start_idx=0):
@@ -215,6 +130,121 @@ def plot_coef_history(x_hat_hist, P_hat_hist, sh_EGM2008, remove_deg, start_idx=
             else:
                 l += 1
                 m = 0
+
+
+def test_setup(max_true_degree, regress_degree, remove_degree, P0_diag, initial_batch):
+    import matplotlib.pyplot as plt
+
+    from GravNN.CelestialBodies.Planets import Earth
+    from GravNN.GravityModels.SphericalHarmonics import SphericalHarmonics, get_sh_data
+    from GravNN.Trajectories import DHGridDist
+
+    planet = Earth()
+
+    MAX_TRUE_DEG = max_true_degree
+    REGRESS_DEG = regress_degree
+    REMOVE_DEG = remove_degree
+
+    def compute_dimensionality(N, M):
+        return (N + 2) * (N + 1) - (M + 2) * (M + 1)
+
+    dim = compute_dimensionality(REGRESS_DEG, REMOVE_DEG)
+
+    sh_EGM2008 = SphericalHarmonics(planet.sh_file, REGRESS_DEG)
+
+    trajectory = DHGridDist(planet, sh_EGM2008.radEquator, 90)
+    # trajectory = RandomDist(planet, [planet.radius, planet.radius + 420000], 100000)
+
+    x, a, u = get_sh_data(
+        trajectory,
+        planet.sh_file,
+        max_deg=MAX_TRUE_DEG,
+        deg_removed=REMOVE_DEG,
+    )
+
+    x0 = np.zeros((dim,))
+    x0[0] = 0.0 if REMOVE_DEG != -1 else 1.0
+
+    # Define uncertainty
+    P0 = np.identity(dim) * P0_diag
+
+    # Functionally no meas uncertainty, but 1e-16 needed to avoid singularities
+    Rk = np.identity(3) + 1e-16
+
+    # Initialize the regressor
+    regressor = RLLS(REGRESS_DEG, planet, x0, P0, Rk, REMOVE_DEG)
+    regressor.update(x, a, init_batch=initial_batch, history=True)
+
+    # Format the coefficients
+    C_lm, S_lm = format_coefficients(regressor.x_hat, regressor.N, REMOVE_DEG)
+    C_lm, S_lm = populate_removed_degrees(
+        C_lm,
+        S_lm,
+        sh_EGM2008.C_lm,
+        sh_EGM2008.S_lm,
+        REMOVE_DEG,
+    )
+
+    # Compute Error + Metrics
+    k = len(C_lm)
+    C_lm_true = sh_EGM2008.C_lm[:k, :k]
+    S_lm_true = sh_EGM2008.S_lm[:k, :k]
+
+    C_lm_error = (C_lm_true - C_lm) / C_lm_true * 100
+    S_lm_error = (S_lm_true - S_lm) / S_lm_true * 100
+
+    C_lm_error[np.isinf(C_lm_error)] = np.nan
+    S_lm_error[np.isinf(S_lm_error)] = np.nan
+
+    C_lm_avg_error = np.nanmean(np.abs(C_lm_error))
+    S_lm_avg_error = np.nanmean(np.abs(S_lm_error))
+
+    with tempfile.NamedTemporaryFile() as tmpfile:
+        save(tmpfile.name, planet, C_lm, S_lm)
+        regressed_model = SphericalHarmonics(tmpfile.name, REGRESS_DEG)
+        accelerations = regressed_model.compute_acceleration(trajectory.positions)
+
+        x, a, u = get_sh_data(
+            trajectory,
+            planet.sh_file,
+            max_deg=MAX_TRUE_DEG,
+            deg_removed=-1,
+        )
+        a_error = (
+            np.linalg.norm(accelerations - a, axis=1) / np.linalg.norm(a, axis=1) * 100
+        )
+
+        print(f"\n AVERAGE CLM ERROR: {C_lm_avg_error} \n")
+        print(f"\n AVERAGE SLM ERROR: {S_lm_avg_error} \n")
+
+        print(f"\n ACCELERATION ERROR: {np.mean(a_error)}")
+
+    # plot_coef_history(regressor.x_hat_hist, regressor.P_hat_hist, sh_EGM2008, REMOVE_DEG, start_idx=0)
+
+    plt.show()
+    return
+
+
+def main():
+    # # Slightly wrong
+    a_error = test_setup(
+        max_true_degree=10,
+        regress_degree=4,
+        remove_degree=1,
+        P0_diag=0,  # assume only uncertainty in measurements (not in initial guess)
+        initial_batch=1000,
+    )
+    assert np.isclose(a_error, 0.010926797140351335)
+
+    # Perfect
+    a_error = test_setup(
+        max_true_degree=4,
+        regress_degree=4,
+        remove_degree=-1,
+        P0_diag=0,
+        initial_batch=500,
+    )
+    assert np.isclose(a_error, 2.705730413079536e-11)
 
 
 if __name__ == "__main__":
