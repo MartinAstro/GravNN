cf1198970e553e90b2e703b8f69c3d0a9d087ce0
John M <john.r.martin@colorado.edu>
2023-11-06 12:04:28 -0500

    ELM MVP

--- GravNN/Regression/ELM.py
+++ GravNN/Regression/ELM.py
@@ -1,149 +1,159 @@
 import numpy as np
-import tensorflow as tf
+from sklearn.preprocessing import MinMaxScaler
+
+np.random.seed(1234)
 
 
-# Inspired from:
-# https://github.com/burnpiro/elm-pure
-# https://github.com/otenim/TensorFlow-OS-ELM/blob/master/train_mnist.py
 class OS_ELM:
-    def __init__(self, n_input_nodes, n_hidden_nodes, n_output_nodes):
+    def __init__(self, n_input_nodes, n_hidden_nodes, n_output_nodes, k=1):
         self.n_input_nodes = n_input_nodes
         self.n_hidden_nodes = n_hidden_nodes
         self.n_output_nodes = n_output_nodes
-        self.activation = tf.nn.tanh
-        self.lossfun = tf.losses.mean_squared_error
+        self.activation = lambda x: 1 / (1 + np.exp(-x))
+        self.input_scaler = MinMaxScaler()
+        self.output_scaler = MinMaxScaler()
 
-        self.w = tf.Variable(
-            tf.random.uniform(
-                [self.n_input_nodes, self.n_hidden_nodes],
-                -1,
-                1,
-                seed=1234,
-            ),
-            name="w",
-            trainable=False,
+        self.w = np.random.uniform(
+            -1,
+            1,
+            size=(self.n_hidden_nodes, self.n_input_nodes),
         )
-        self.beta = tf.Variable(
-            tf.random.uniform(
-                [self.n_hidden_nodes, self.n_output_nodes],
-                -1,
-                1,
-                seed=1234,
-            ),
-            name="beta",
-            trainable=False,
-        )
-        self.bias = tf.Variable(
-            tf.random.uniform([self.n_hidden_nodes], -1, 1, seed=1234),
-            name="bias",
-            trainable=False,
-        )
-        self.P = tf.Variable(
-            tf.zeros([self.n_hidden_nodes, self.n_hidden_nodes]),
-            name="p",
-            trainable=False,
-        )
+        self.bias = np.random.uniform(-1, 1, size=(self.n_hidden_nodes, 1))
+        self.beta = np.zeros(shape=(self.n_output_nodes, self.n_hidden_nodes))
+        self.c = 1.0 / k
+        self.K_inv = np.zeros(shape=(self.n_hidden_nodes, self.n_hidden_nodes))
 
     def predict(self, x):
-        H = self.activation(x @ self.w + self.bias)
-        y = H @ self.beta
+        if x.ndim == 1:
+            x = x.reshape(1, -1)
+        if x.shape[0] != self.n_input_nodes:
+            x = x.T
+
+        x_nd = self.input_scaler.transform(x.T).T
+
+        H = self.activation(self.w * x_nd + self.bias)
+        y_nd = self.beta @ H
+
+        y = self.output_scaler.inverse_transform(y_nd.T).T
         return y
 
     def init_train(self, x, y):
         # * is element wise
         # @ is matrix wise
 
-        # Works
-        H = self.activation(x @ self.w + self.bias)
-        H_pinv = tf.linalg.pinv(H)
-        self.beta.assign(H_pinv @ y)
+        # X [MxN]
+        # W [HxM]
+        # H = [HxN]
+        # Beta [TxH]
+        # Y [TxN]
 
-        # Doesn't
-        HT = tf.transpose(H)
-        HTH_inv = tf.linalg.inv(HT @ H)
-        H_inv2 = HTH_inv @ HT
-        self.P.assign(HTH_inv)
-        self.beta.assign(H_inv2 @ y)
+        # Normalize the inputs and outputs using sklearn
+        x = self.input_scaler.fit_transform(x.T).T
+        y = self.output_scaler.fit_transform(y.T).T
 
-        # HT = tf.transpose(H)
-        # # self.P.assign(tf.linalg.pinv(H))
-        # self.P.assign(tf.linalg.inv(HT @ H))
-        # PHT = self.P @ HT
-        # self.beta.assign(PHT @ y)
-        return
+        H = self.activation(self.w * x + self.bias)
+        Ht = np.transpose(H)
+        K = H @ Ht + np.eye((len(H)), dtype=H.dtype) / self.c
+        K_inv = np.linalg.inv(K)
+
+        Yt = np.transpose(y)
+        beta_t = K_inv @ H @ Yt
+        beta = np.transpose(beta_t)
+
+        self.beta = beta
+        self.K_inv = K_inv
 
     def seq_train(self, x, y):
-        H = self.activation(x @ self.w + self.bias)
-        HT = tf.transpose(H)
-        batch_size = tf.shape(x)[0]
-        I = tf.eye(batch_size)
-        Hp = tf.matmul(H, self.P)
-        HpHT = tf.matmul(Hp, HT)
-        temp = tf.linalg.inv(I + HpHT)
-        PHT = tf.matmul(self.P, HT)
-        self.P.assign(self.P - tf.matmul(tf.matmul(PHT, temp), Hp))
-        PHT = tf.matmul(self.P, HT)
-        Hbeta = tf.matmul(H, self.beta)
-        self.beta.assign(self.beta + tf.matmul(PHT, y - Hbeta))
-        return
+        # Normalize the inputs and outputs using sklearn
+        x = self.input_scaler.transform(x.T).T
+        y = self.output_scaler.transform(y.T).T
+
+        # If you want to solve for Beta, then transpose everything
+        H = self.activation(self.w * x + self.bias)
+        Ht = np.transpose(H)
+        Kk_inv = self.K_inv
+
+        eye = np.eye(len(Ht), dtype=H.dtype)
+
+        intermediate = np.linalg.inv(eye + Ht @ Kk_inv @ H)
+        dK = Kk_inv @ H @ intermediate @ Ht @ Kk_inv
+        new_K_inv = Kk_inv - dK
+
+        # Correct Beta
+        y_hat = self.predict(x)
+        dy = y - y_hat
+        dBeta = new_K_inv @ H @ np.transpose(dy)
+        new_beta = self.beta + np.transpose(dBeta)
+
+        self.beta = new_beta
+        self.K_inv = new_K_inv
+
+    def update(self, x, y, init_batch):
+        self.init_train(x[:init_batch], y[:init_batch])
+        for i in range(init_batch, len(x), init_batch):
+            self.seq_train(x[i : i + init_batch], y[i : i + init_batch])
 
 
 def main():
-    np.random.seed(1234)
-
     n_input_nodes = 1
     n_hidden_nodes = 100
     n_output_nodes = 1
 
-    N = 1000
+    N = 5000
     N_test = 100
-    init_batch = N
+    init_batch = 1000
+
+    # Regularization Factor C = 5*10**5
+    # L = 5*10**4
+
+    # C = 1/k, therefore k = 2E-6
 
     os_elm = OS_ELM(
         n_input_nodes=n_input_nodes,
         n_hidden_nodes=n_hidden_nodes,
         n_output_nodes=n_output_nodes,
+        k=2e-6,
     )
 
     def fcn(x):
         # return np.sin(5 * x)
-        return x**2
+        return x**2  # + np.sin(5*x)
         return x
 
     def noisy_fcn(x):
-        return fcn(x) + 0.01 * np.random.rand(*x.shape)
+        return fcn(x) + 0.01 * np.random.uniform(x.shape)
 
-    x = np.random.rand(N, n_input_nodes).astype(np.float32)
+    x = np.random.uniform(size=(N, n_input_nodes))
     y = fcn(x)
 
-    x_test = np.random.rand(N_test, n_input_nodes).astype(np.float32)
+    x_test = np.random.uniform(size=(N_test, n_input_nodes))
     y_test = fcn(x_test)
 
     x_init = x[:init_batch]
     y_init = y[:init_batch]
-    os_elm.init_train(x_init, y_init)
+    os_elm.init_train(x_init.T, y_init.T)
 
-    y_hat = os_elm.predict(x_test)
+    y_hat = os_elm.predict(x_test.T)
     L = np.mean(np.square(y_test - y_hat))
     print(L)
 
-    # # iterate through remainder of the vectors in batches of 10
-    # step = 1000
-    # for i in range(init_batch, len(x), step):
-    #     x_seq = x[i:i+step]
-    #     y_seq = y[i:i+step]
-    #     os_elm.seq_train(x_seq, y_seq)
+    # iterate through remainder of the vectors in batches of 10
+    step = 1000
+    for i in range(init_batch, len(x), step):
+        x_seq = x[i : i + step]
+        y_seq = y[i : i + step]
+        os_elm.seq_train(x_seq.T, y_seq.T)
 
-    # # 'predict' method returns raw values of output nodes.
-    # y_hat = os_elm.predict(x_test)
-    # L = np.mean(np.square(y_test - y_hat))
-    # print(L)
+    # 'predict' method returns raw values of output nodes.
+    y_hat = os_elm.predict(x_test)
+    L = np.mean(np.square(y_test - y_hat))
+    print(L)
 
     import matplotlib.pyplot as plt
 
     plt.figure()
     plt.scatter(x, y, s=2)
-    plt.scatter(x, os_elm.predict(x), s=2)
+    plt.scatter(x, os_elm.predict(x.T), s=2)
 
     # plt.figure()
     # plt.scatter(x_test, y_test, s=2)
