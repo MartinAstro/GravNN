5b5be6a679e9db01bd70410c02052a5eaac71425
John M <john.r.martin@colorado.edu>
2023-11-21 15:03:41 -0500

    Add run script

---
+++ Scripts/HPC/run_comparison.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --array=0-64
+#SBATCH --account=ucb387_asc1
+#SBATCH --time=02:00:00
+#SBATCH --output=SlurmFiles/Comparison/comparison-%j.out
+module purge
+
+echo "== Load Anaconda =="
+
+module load slurm/alpine
+module load anaconda
+
+echo "== Activate Env =="
+
+conda activate research
+
+echo "== Run data generation =="
+
+srun python /projects/joma5012/GravNN/Scripts/Comparison/train_HPC.py $SLURM_ARRAY_TASK_ID
+
+wait # Necessary to wait for all processes to finish
+echo "== End of Job =="
+exit 0
