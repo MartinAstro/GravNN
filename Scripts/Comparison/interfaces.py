30f4b8402a238896d0357a7e653b6a3df4524fe1
John M <john.r.martin@colorado.edu>
2023-11-21 14:37:14 -0500

    Update interfaces

--- Scripts/Comparison/interfaces.py
+++ Scripts/Comparison/interfaces.py
@@ -1,5 +1,7 @@
+import copy
 import os
 import pickle
+import time
 from pathlib import Path
 
 import numpy as np
@@ -7,7 +9,9 @@ import numpy as np
 import GravNN
 from GravNN.Analysis.ExtrapolationExperiment import ExtrapolationExperiment
 from GravNN.Analysis.PlanesExperiment import PlanesExperiment
+from GravNN.Analysis.SurfaceExperiment import SurfaceExperiment
 from GravNN.Analysis.TrajectoryExperiment import TestModel, TrajectoryExperiment
+from GravNN.GravityModels.ELM import ELM
 from GravNN.GravityModels.HeterogeneousPoly import (
     generate_heterogeneous_model,
 )
@@ -23,10 +27,34 @@ from GravNN.Networks.utils import (
     configure_tensorflow,
 )
 from GravNN.Regression.BLLS import BLLS_PM, format_coefficients
-from GravNN.Regression.ELM import OS_ELM
+from GravNN.Regression.ELMRegressor import OS_ELM
 from GravNN.Regression.MasconRegressor import MasconRegressor
-from GravNN.Regression.RLLS import RLLS
+from GravNN.Regression.SHRegression import SHRegressorSequential
 from GravNN.Regression.utils import save
+
+
+# Decorators
+def time_train_method(func):
+    def wrapper(self, *args, **kwargs):
+        start_time = time.time()
+        result = func(self, *args, **kwargs)
+        end_time = time.time()
+        self.train_duration = end_time - start_time
+        return result
+
+    return wrapper
+
+
+def save_time(func):
+    def wrapper(self, *args, **kwargs):
+        result = func(self, *args, **kwargs)
+        filename = self.filename
+        time_file = os.path.splitext(filename)[0] + "_time.data"
+        with open(str(time_file), "wb") as f:
+            pickle.dump(self.train_duration, f)
+        return result
+
+    return wrapper
 
 
 class ModelInterface:
@@ -34,28 +62,38 @@ class ModelInterface:
         "Configure the gravity model regressor"
         pass
 
-    def train(self, data):
-        "Fit the gravity model to the data"
-        pass
-
-    def save(self):
-        "Save the fit gravity model"
-        pass
-
     def load(self):
         "Take the configuration data, locate idx, and load trained model"
+        raise NotImplementedError("Must implement load method")
 
     def get_model(self):
         "Get the gravity model that can be evaluated"
-        pass
+        raise NotImplementedError("Must implement get_model method")
 
     def count_params(self):
         "Function that computes number of parameters in model"
-        pass
+        raise NotImplementedError("Must implement count_params method")
+
+    def save_location(self, unique_idx, ext):
+        directory = Path(os.path.abspath(os.path.dirname(GravNN.__file__)))
+        filename = directory / f"../Data/Comparison/{unique_idx}.{ext}"
+        os.makedirs(os.path.dirname(filename), exist_ok=True)
+        self.filename = str(filename)
+
+    def train(self, data):
+        "Fit the gravity model to the data"
+        raise NotImplementedError("Must implement train method")
 
     def evaluate(self, override=False):
         model = self.get_model()
-        exp = ExtrapolationExperiment(model, self.config, points=5000)
+        exp = ExtrapolationExperiment(
+            model,
+            self.config,
+            points=4500,
+            extrapolation_bound=10,
+        )
+        exp.test_dist_2_surf_idx = None  # hack to avoid needing to reorder indices
+        exp.test_r_surf = None
         exp.run(override)
         self.extrap_exp = exp
 
@@ -64,13 +102,13 @@ class ModelInterface:
         exp = PlanesExperiment(
             model,
             self.config,
-            bounds=[-3 * R, 3 * R],
-            samples_1d=100,
+            bounds=[-5 * R, 5 * R],
+            samples_1d=200,
         )
         exp.run(override)
         self.plane_exp = exp
 
-        true_model = generate_heterogeneous_model(planet, planet.obj_8k)
+        true_model = generate_heterogeneous_model(planet, planet.obj_200k)
         initial_state = np.array(
             [
                 2.88000000e04,
@@ -89,33 +127,39 @@ class ModelInterface:
             true_model,
             [test_model],
             initial_state=initial_state,
+            pbar=True,
             period=period,
             t_mesh_density=1000,
             omega_vec=omega_vec,
         )
         exp.run(override)
         self.trajectory_exp = exp
+
+        surface_exp = SurfaceExperiment(model, true_model)
+        surface_exp.run(override)
+        self.surface_exp = surface_exp
+
         self.params = self.count_params()
+
+    def save(self):
+        "Save the fit gravity model"
+        raise NotImplementedError("Must implement save method")
 
 
 class SphericalHarmonicWrapper(ModelInterface):
     def configure(self, config):
         degree = config["deg"][0]
         self.planet = config["planet"][0]
-        # self.regressor = BLLS(degree, self.planet)
-        # self.regressor = RLLS(degree, self.planet)
+        model_name = config["model_name"][0]
         self.config = config
         self.degree = degree
 
         N_train = self.config["N_train"][0]
         acc_noise = self.config["acc_noise"][0]
-        unique_idx = f"{self.degree}_{N_train}_{acc_noise}"
-        directory = Path(os.path.abspath(os.path.dirname(GravNN.__file__)))
-        self.filename = (
-            directory / f"Files/GravityModels/SH/BLLS/Comparison/{unique_idx}.txt"
-        )
-        self.filename = str(self.filename)
+        unique_idx = f"{model_name}_{self.degree}_{N_train}_{acc_noise}"
+        self.save_location(unique_idx, "txt")
 
+    @time_train_method
     def train(self, dataset):
         x = dataset.raw_data["x_train"]
         a = dataset.raw_data["a_train"]
@@ -123,22 +167,10 @@ class SphericalHarmonicWrapper(ModelInterface):
         # Configure Parameters
         N = self.degree
         REMOVE_DEG = -1
-        P0_diag = 0
-        dim = (N + 2) * (N + 1) - (REMOVE_DEG + 2) * (REMOVE_DEG + 1)
-        init_batch = 1000 if len(x) > 1000 else len(x)
-        # Configure State
-        x0 = np.zeros((dim,))
-        x0[0] = 0.0 if REMOVE_DEG != -1 else 1.0
-
-        # Define uncertainty
-        P0 = np.identity(dim) * P0_diag
-
-        # Functionally no meas uncertainty, but 1e-16 needed to avoid singularities
-        Rk = np.identity(3) + 1e-16
 
         # Initialize the regressor
-        self.regressor = RLLS(N, self.planet, x0, P0, Rk, REMOVE_DEG)
-        results = self.regressor.update(x, a, init_batch=init_batch, history=True)
+        self.regressor = SHRegressorSequential(N, max_param=5000, planet=self.planet)
+        results = self.regressor.update(x, a)
 
         self.C_lm, self.S_lm = format_coefficients(results, self.degree, REMOVE_DEG)
         self.results = results
@@ -146,6 +178,7 @@ class SphericalHarmonicWrapper(ModelInterface):
     def load(self):
         self.model = self.get_model()
 
+    @save_time
     def save(self):
         save(self.filename, self.planet, self.C_lm, self.S_lm)
 
@@ -161,19 +194,17 @@ class PMWrapper(SphericalHarmonicWrapper):
     def configure(self, config):
         degree = 0
         self.planet = config["planet"][0]
+        model_name = config["model_name"][0]
         self.regressor = BLLS_PM(degree, self.planet)
         self.config = config
         self.degree = degree
 
         N_train = self.config["N_train"][0]
         acc_noise = self.config["acc_noise"][0]
-        unique_idx = f"{self.degree}_{N_train}_{acc_noise}"
-        directory = Path(os.path.abspath(os.path.dirname(GravNN.__file__)))
-        self.filename = (
-            directory / f"Files/GravityModels/PM/BLLS/Comparison/{unique_idx}.txt"
-        )
-        self.filename = str(self.filename)
+        unique_idx = f"{model_name}_{self.degree}_{N_train}_{acc_noise}"
+        self.save_location(unique_idx, "txt")
 
+    @time_train_method
     def train(self, dataset):
         x = dataset.raw_data["x_train"]
         a = dataset.raw_data["a_train"]
@@ -189,7 +220,7 @@ class PMWrapper(SphericalHarmonicWrapper):
     def get_model(self):
         SH_model = SphericalHarmonics(self.filename, self.degree)
         mu = SH_model.C_lm[0, 0] * self.planet.mu
-        planet = self.config["planet"][0]
+        planet = copy.deepcopy(self.config["planet"][0])
         planet.mu = mu
         PM_model = PointMass(planet)
         self.model = PM_model
@@ -203,10 +234,15 @@ class PolyhedralWrapper(ModelInterface):
     def configure(self, config):
         self.config = config
         self.shape = config["shape"][0]
+        self.model_name = config["model_name"][0]
+        unique_idx = f"{self.model_name}_{self.shape}"
+        self.save_location(unique_idx, "txt")
 
+    @time_train_method
     def train(self, dataset):
         pass
 
+    @save_time
     def save(self):
         pass
 
@@ -220,79 +256,42 @@ class PolyhedralWrapper(ModelInterface):
 
     def count_params(self):
         return (
-            self.mesh.vertices.shape[0] * 3 + self.mesh.faces.shape[0] * 3 / 2
+            self.model.mesh.vertices.shape[0] * 3
+            + self.model.mesh.faces.shape[0] * 3 / 2
         )  # Float + long int
 
 
 class PINNWrapper(ModelInterface):
     def configure(self, config):
         hparams = {
-            "learning_rate": [0.0001],
+            "learning_rate": [0.001],
             "batch_size": [2**11],
-            "epochs": [20000],
+            "epochs": [8192],
         }
         configure_run_args(config, hparams)
         configure_tensorflow(config)
         self.model = PINNGravityModel(config)
-
-        directory = Path(os.path.abspath(os.path.dirname(GravNN.__file__)))
+        self.config = config
         N_train = self.config["N_train"][0]
         acc_noise = self.config["acc_noise"][0]
         num_units = self.config["num_units"][0]
         model_name = self.config["model_name"][0]
 
-        unique_idx = f"{num_units}_{N_train}_{acc_noise}_{model_name}"
-        self.filename = directory + f"../Data/Comparison/{unique_idx}.data"
+        unique_idx = f"{model_name}_{num_units}_{N_train}_{acc_noise}"
+        self.save_location(unique_idx, "data")
 
+    @time_train_method
     def train(self, dataset):
         self.model.train(dataset)
 
+    @save_time
     def save(self):
         saver = ModelSaver(self.model)
         saver.save(self.filename)
 
     def load(self):
         config, model = load_config_and_model(self.filename, idx=-1)
-        self.model = model
-
-    def get_model(self):
-        return self.model
-
-    def count_params(self):
-        return count_nonzero_params(self.model)
-
-
-class NNWrapper(ModelInterface):
-    def configure(self, config):
-        hparams = PINN_I()
-        hparams.update(ReduceLrOnPlateauConfig())
-        hparams.update(
-            {
-                "epochs": [20000],
-            },
-        )
-        configure_run_args(config, hparams)
-        configure_tensorflow(config)
-        self.model = PINNGravityModel(config)
-
-        directory = Path(os.path.abspath(os.path.dirname(GravNN.__file__)))
-        N_train = self.config["N_train"][0]
-        acc_noise = self.config["acc_noise"][0]
-        num_units = self.config["num_units"][0]
-        model_name = self.config["model_name"][0]
-
-        unique_idx = f"{num_units}_{N_train}_{acc_noise}_{model_name}"
-        self.filename = directory + f"../Data/Comparison/{unique_idx}.data"
-
-    def train(self, dataset):
-        self.model.train(dataset)
-
-    def save(self):
-        saver = ModelSaver(self.model)
-        saver.save(self.filename)
-
-    def load(self):
-        config, model = load_config_and_model(self.filename, idx=-1)
+        self.config = config
         self.model = model
 
     def get_model(self):
@@ -305,18 +304,23 @@ class NNWrapper(ModelInterface):
 class MasconWrapper(ModelInterface):
     def configure(self, config):
         planet = config["planet"][0]
+        model_name = config["model_name"][0]
         obj_file = config["obj_file"][0]
         N_masses = config["elements"][0]
-        acc_noise = self.config["acc_noise"][0]
-        N_train = self.config["N_train"][0]
-        self.filename = f"{N_masses}_{N_train}_{acc_noise}.csv"
+        acc_noise = config["acc_noise"][0]
+        N_train = config["N_train"][0]
+        self.config = config
         self.regressor = MasconRegressor(planet, obj_file, N_masses)
+        unique_idx = f"{model_name}_{N_masses}_{N_train}_{acc_noise}"
+        self.save_location(unique_idx, "csv")
 
+    @time_train_method
     def train(self, dataset):
         x = dataset.raw_data["x_train"]
         a = dataset.raw_data["a_train"]
         self.regressor.update(x, a)
 
+    @save_time
     def save(self):
         self.regressor.save(self.filename)
 
@@ -333,9 +337,13 @@ class MasconWrapper(ModelInterface):
 
 
 class ELMWrapper(ModelInterface):
+    def __init__(self):
+        super().__init__()
+
     def configure(self, config):
         configure_tensorflow(config)
-        self.model = OS_ELM(
+        self.config = config
+        self.regressor = OS_ELM(
             n_input_nodes=3,
             n_hidden_nodes=config["num_units"][0],
             n_output_nodes=3,
@@ -346,29 +354,25 @@ class ELMWrapper(ModelInterface):
         num_units = self.config["num_units"][0]
         model_name = self.config["model_name"][0]
 
-        directory = Path(os.path.abspath(os.path.dirname(GravNN.__file__)))
+        unique_idx = f"{model_name}_{num_units}_{N_train}_{acc_noise}"
+        self.save_location(unique_idx, "data")
 
-        unique_idx = f"{num_units}_{N_train}_{acc_noise}_{model_name}"
-        self.filename = directory + f"../Data/Comparison/{unique_idx}.data"
-        unique_idx = f"{num_units}_{N_train}_{acc_noise}_{model_name}"
-        self.filename = directory + f"../Data/Comparison/elm_{unique_idx}.data"
-
+    @time_train_method
     def train(self, dataset):
         x = dataset.raw_data["x_train"]
         a = dataset.raw_data["a_train"]
-        self.model.update(x, a)
+        self.regressor.update(x, a, init_batch=1000)
 
+    @save_time
     def save(self):
         # save model to pickle
-        with open(self.filename, "wb") as f:
-            pickle.dump(self.model, f)
+        self.regressor.save(self.filename)
 
     def load(self):
-        # load pickle
-        with open(self.filename, "rb") as f:
-            self.model = pickle.load(f)
+        self.model = self.get_model()
 
     def get_model(self):
+        self.model = ELM(self.filename)
         return self.model
 
     def count_params(self):
@@ -386,10 +390,8 @@ def make_experiments(exp_list):
 
 
 def select_model(model_name):
-    if "PINN" in model_name.upper():
+    if "PINN" in model_name.upper() or "TNN" in model_name.upper():
         return PINNWrapper()
-    if "TNN" in model_name.upper():  # traditional NN
-        return NNWrapper()
     elif model_name.upper() == "MASCONS":
         return MasconWrapper()
     elif model_name.upper() == "POLYHEDRAL":
