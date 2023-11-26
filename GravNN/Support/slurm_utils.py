9599ccadffbfe730bc7fb1102b2f85cab0fd26a4
John M <john.r.martin@colorado.edu>
2023-11-26 10:09:20 -0500

    Update Slurm Utils

--- GravNN/Support/slurm_utils.py
+++ GravNN/Support/slurm_utils.py
@@ -15,11 +15,14 @@ def get_available_cores():
 
 
 def print_slurm_info():
-    num_nodes = int(os.environ["SLURM_JOB_NUM_NODES"])
-    cores_per_nodes = int(os.environ["SLURM_JOB_CPUS_PER_NODE"].split("(")[0])
-    num_threads = os.cpu_count()
+    try:
+        num_nodes = int(os.environ["SLURM_JOB_NUM_NODES"])
+        cores_per_nodes = int(os.environ["SLURM_JOB_CPUS_PER_NODE"].split("(")[0])
+        num_threads = os.cpu_count()
 
-    print(f"Cores Per Node:{os.environ['SLURM_JOB_CPUS_PER_NODE']}")
-    print(f"Num Nodes:{os.environ['SLURM_JOB_NUM_NODES']}")
-    print(f"Available Cores:{num_nodes * cores_per_nodes}")
-    print(f"Threads per core:{num_threads / (cores_per_nodes * num_nodes)}")
+        print(f"Cores Per Node:{os.environ['SLURM_JOB_CPUS_PER_NODE']}")
+        print(f"Num Nodes:{os.environ['SLURM_JOB_NUM_NODES']}")
+        print(f"Available Cores:{num_nodes * cores_per_nodes}")
+        print(f"Threads per core:{num_threads / (cores_per_nodes * num_nodes)}")
+    except Exception:
+        pass
