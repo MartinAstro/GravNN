16e4a6fde45f1e772fee5f9f8b4cd3f1249bb187
John M <john.r.martin@colorado.edu>
2023-11-29 17:33:39 -0500

    Update Train

--- Scripts/Train/train.py
+++ Scripts/Train/train.py
@@ -1,4 +1,3 @@
-import multiprocessing as mp
 import os
 from pprint import pprint
 
@@ -11,49 +10,40 @@ os.environ["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] = "YES"
 
 
 def main():
-    threads = 1
-
     df_file = "Data/Dataframes/eros_072223.data"
-    # df_file = "Data/Dataframes/fourier_tests_updated.data"
     config = get_default_eros_config()
     config.update(PINN_III())
     config.update(ReduceLrOnPlateauConfig())
 
     hparams = {
-        "N_dist": [60000],
-        "N_train": [50000],
+        "N_dist": [50000],
+        "N_train": [2**15],
         "N_val": [4096],
-        "num_units": [20],
-        "loss_fcns": [["percent", "rms"]],
+        "num_units": [16],
+        "layers": [[3, 1, 1, 1, 1, 1, 1, 3]],
+        "loss_fcns": [["percent", "mse"]],
         "jit_compile": [True],
         "lr_anneal": [False],
         "eager": [False],
         "learning_rate": [0.0001],
         "dropout": [0.0],
-        "batch_size": [2**11],
+        # "batch_size": [2**11],
+        "batch_size": [2**16],
         # "batch_size": [4500],
-        "epochs": [20000],
+        "epochs": [200],
         "acc_noise": [0.0],
         "gravity_data_fcn": [get_hetero_poly_data],
-        # "network_arch" : ["traditional"],
-        # "preprocessing" : [["pines", "r_inv", "fourier_2n"]],
-        # "preprocessing" : [["pines", "r_inv", "fourier"]],
         "preprocessing": [["pines", "r_inv"]],
         "PINN_constraint_fcn": ["pinn_a"],
-        # "init_file" : [2460020.362463469],
-        # "fourier_features" : [3],
-        # "fourier_sigma" : [2],
-        # "freq_decay": [True],
-        # "trainable_tanh" : [True],
-        # "tanh_r" : [3],
-        # "tanh_k": [5],
+        "tanh_k": [0.1],
+        "early_stop": [True],
     }
     args = configure_run_args(config, hparams)
 
-    # run(*args[0])
-    with mp.Pool(threads) as pool:
-        results = pool.starmap_async(run, args)
-        configs = results.get()
+    configs = [run(*args[0])]
+    # with mp.Pool(threads) as pool:
+    #     results = pool.starmap_async(run, args)
+    #     configs = results.get()
     save_training(df_file, configs)
 
     # from Scripts.Figures.Earth.nn_brillouin_map import main as main_maps
@@ -78,6 +68,8 @@ def run(config):
     data = DataSet(config)
     model = PINNGravityModel(config)
     history = model.train(data)
+    model.config["val_loss"] = history.history["val_percent_mean"][-1]
+
     saver = ModelSaver(model, history)
     saver.save(df_file=None)
 
