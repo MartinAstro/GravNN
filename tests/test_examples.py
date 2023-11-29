590aac45de817328ddb7a6a90a75d69d5aa4472f
John M <john.r.martin@colorado.edu>
2023-11-29 18:19:54 -0500

    Remove examples test

--- tests/test_examples.py
+++
@@ -1,8 +0,0 @@
-from ..Examples import analyze_network, load_network, plot_gravity_map, train_network
-
-
-def test_run_examples():
-    train_network()
-    load_network()
-    analyze_network()
-    plot_gravity_map()
