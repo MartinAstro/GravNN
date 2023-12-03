b8f2b3ed40988d8bbd03da741c32cd76c550a3ab
John M <john.r.martin@colorado.edu>
2023-12-03 15:30:37 -0500

    Investigate the potential along different axes and how they differ from PM

--- Scripts/Figures/Eros/potential_model.py
+++ Scripts/Figures/Eros/potential_model.py
@@ -1,69 +1,123 @@
-import os
-
 import matplotlib.pyplot as plt
-import pandas as pd
-import StatOD
-from StatOD.utils import *
+import numpy as np
 
+from GravNN.CelestialBodies.Asteroids import Eros
 from GravNN.GravityModels.HeterogeneousPoly import generate_heterogeneous_model
-from GravNN.Networks.Configs import *
+from GravNN.GravityModels.PointMass import PointMass
 from GravNN.Networks.Model import load_config_and_model
 
-os.environ["OBJC_DISABLE_INITIALIZE_FORK_SAFETY"] = "YES"
 
+def plot_analytic_model(fcn, min_R, max_R, radius, label, new_fig=True):
+    r = np.linspace(min_R, max_R, 1000)
+    x_array = np.zeros((r.shape[0], 3))
+    y_array = np.zeros((r.shape[0], 3))
+    z_array = np.zeros((r.shape[0], 3))
 
-def plot_analytic_model(gravity_model):
-    config = gravity_model.config
-    planet = config["planet"][0]
-    x_coordinates = np.linspace(1, 2 * config["planet"][0].radius, 200)
-    positions = np.zeros((x_coordinates.shape[0], 3))
-    positions[:, 1] = x_coordinates
+    x_array[:, 0] = r
+    y_array[:, 1] = r
+    z_array[:, 2] = r
 
-    analytic_potential = gravity_model.compute_potential(positions).numpy().squeeze()
-    u_point_mass = -planet.mu / x_coordinates
-    u_point_mass_mod = (-planet.mu / x_coordinates) * (1 / x_coordinates) ** 0.75
+    U_x = fcn(x_array)
+    U_y = fcn(y_array)
+    U_z = fcn(z_array)
 
-    plt.figure()
-    plt.subplot(3, 1, 1)
-    plt.plot(x_coordinates, analytic_potential, label="Analytic Model")
-    plt.plot(x_coordinates, u_point_mass, label="1/r")
-    plt.plot(x_coordinates, u_point_mass_mod, label="1/r-mod")
-    plt.ylim([-60, 0])
+    plt.figure(1)
+    plt.plot(r / radius, U_x, label=f"{label} x")
+    plt.xlabel("x")
 
-    poly_gm = generate_heterogeneous_model(planet, planet.obj_8k)
-    poly_potential = poly_gm.compute_potential(positions)
-    plt.subplot(3, 1, 2)
-    plt.plot(x_coordinates, poly_potential, label="Poly Model")
-    plt.plot(x_coordinates, u_point_mass, label="1/r")
-    plt.plot(x_coordinates, u_point_mass_mod, label="1/r-mod")
+    plt.figure(2)
+    plt.plot(r / radius, U_y, label=f"{label} y")
+    plt.xlabel("y")
 
-    # plt.plot(x_coordinates, -(planet.radius/x_coordinates)**0.75, label="1/r-0.75")
+    plt.figure(3)
+    plt.plot(r / radius, U_z, label=f"{label} z")
+    plt.xlabel("z")
 
-    plt.ylim([-60, 0])
+    plt.legend()
 
-    plt.subplot(3, 1, 3)
-    plt.plot(x_coordinates, poly_potential - analytic_potential, label="Poly Model")
-    plt.ylim([-60, 0])
+    return U_x, U_y, U_z, r
 
 
 def main():
-    statOD_dir = os.path.dirname(StatOD.__file__)
-    pinn_name = "eros_pm_061023"
-    # pinn_name = "eros_poly_061023"
-    # pinn_name = "eros_pm_053123"
-    pinn_file = f"{statOD_dir}/../Data/Dataframes/{pinn_name}.data"
+    planet = Eros()
+    poly_model = generate_heterogeneous_model(planet, planet.obj_8k)
+    point_mass_model = PointMass(planet)
 
-    df = pd.read_pickle(pinn_file)
-    config, gravity_model = load_config_and_model(
-        df.id.values[-1],
-        df,
-        custom_dtype="float32",
-        only_weights=True,
+    R_max = planet.radius * 5
+    R_min = planet.radius_min
+    plot_analytic_model(
+        poly_model.compute_potential,
+        R_min,
+        R_max,
+        planet.radius,
+        "Poly",
     )
-    gravity_model.config = config
-    plot_analytic_model(gravity_model)
+    plot_analytic_model(
+        point_mass_model.compute_potential,
+        R_min,
+        R_max,
+        planet.radius,
+        "PM",
+        new_fig=False,
+    )
+
+    # plot_analytic_model(poly_model.compute_acceleration, R_min, R_max, planet.radius, "Poly" )
+    # plot_analytic_model(point_mass_model.compute_acceleration, R_min, R_max, planet.radius, "PM", new_fig=False)
+
+    plt.show()
+
+
+def main_NN():
+    df_file = "Data/Dataframes/eros_cost_fcn_pinn_III_fuse.data"
+    config, model = load_config_and_model(df_file)
+
+    def analytic_model(x):
+        x_input = model.x_preprocessor(x)
+        u_pred = model.analytic_model(x_input, training=False)
+        return u_pred
+
+    def network_model(x):
+        x_input = model.x_preprocessor(x)
+        u_pred = model.network(x_input, training=False)
+        return u_pred
+
+    planet = Eros()
+    R_max = planet.radius * 5
+    R_min = planet.radius_min
+    Ux, Uy, Uz, r = plot_analytic_model(
+        analytic_model,
+        R_min,
+        R_max,
+        planet.radius,
+        "Analytic",
+    )
+    Ux_NN, Uy_NN, Uz_NN, r = plot_analytic_model(
+        network_model,
+        R_min,
+        R_max,
+        planet.radius,
+        "Network",
+        new_fig=False,
+    )
+
+    Ux_err = (Ux - Ux_NN) / Ux
+    Uy_err = (Uy - Uy_NN) / Uy
+    Uz_err = (Uz - Uz_NN) / Uz
+
+    plt.figure()
+    plt.plot(r / planet.radius, Ux_err, label="Ux")
+    plt.plot(r / planet.radius, Uy_err, label="Uy")
+    plt.plot(r / planet.radius, Uz_err, label="Uz")
+    plt.plot(
+        r / planet.radius,
+        np.mean([Ux_err, Uy_err, Uz_err], axis=0),
+        label="Mean",
+        linestyle="--",
+    )
+
     plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    main_NN()
